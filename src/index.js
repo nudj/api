9ca0cb263b39f5e9cd9d0b35469b8de35099b3d4
Nick Collings <nick@collingo.com>
2017-12-01 17:12:14 +0000

    Tidy up loose ends

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const rest = require('./rest')
 const gql = require('./gql')
-const Transaction = require('./gql/arango-adaptor')
+const transaction = require('./gql/arango-adaptor')
 
 rest.listen(81, () => console.log('Rest running on http://localhost:81/'))
-gql({
-  transaction: Transaction({ baseURL: process.env.DB_API_URL })
-}).listen(82, () => console.log('GQL running on http://localhost:82/'))
+gql({ transaction }).listen(82, () => console.log('GQL running on http://localhost:82/'))

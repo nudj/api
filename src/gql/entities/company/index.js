a2d4a0cc876e16be5dd29e70ff9f12368d9ead57
Nick Collings <nick@collingo.com>
2017-12-04 18:14:29 +0000

    Add company properties

---
+++ src/gql/entities/company/index.js
@@ -0,0 +1,7 @@
+const properties = require('./properties')
+const resolvers = require('./resolvers')
+
+module.exports = {
+  typeDefs: [properties],
+  resolvers
+}
---
+++ src/gql/entities/company/properties.js
@@ -0,0 +1,19 @@
+module.exports = `
+  type Company {
+    id: ID!
+    created: DateTime!
+    modified: DateTime!
+    name: String!
+    slug: String!
+    description: String
+    mission: String
+    facebook: String
+    industry: String
+    linkedin: String
+    location: String
+    logo: String
+    size: String
+    twitter: String
+    url: String
+  }
+`

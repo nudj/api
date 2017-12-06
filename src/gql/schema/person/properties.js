ebc56c4718d0d055319fb40be249662d64b3162a
Nick Collings <nick@collingo.com>
2017-12-06 13:44:31 +0000

    Add person properties

--- src/gql/schema/person/properties.js
+++ src/gql/schema/person/properties.js
@@ -2,6 +2,12 @@ module.exports = {
   typeDefs: `
     type Person {
       id: ID!
+      created: DateTime!
+      modified: DateTime!
+      email: String!
+      firstName: String
+      lastName: String
+      url: String
     }
   `,
   resolvers: {}

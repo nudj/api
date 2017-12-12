becde3a1b236e707172cb5240e2c935aeed13524
Nick Collings <nick@collingo.com>
2017-12-12 15:50:48 +0000

    Add old/new gql logic, default to old gql, set onboarded flags to booleans on Hirer/Company

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const rest = require('./rest')
-const gql = require('./gql')
+const gqlNew = require('./gql')
+const gqlOld = require('./gql-old')
 const transaction = require('./gql/adaptors/arango')
 
+const gql = process.env.USE_NEW_API === 'true' ? gqlNew : gqlOld
+
 rest.listen(81, () => console.log('Rest running on http://localhost:81/'))
-gql({ transaction }).listen(82, () => console.log('GQL running on http://localhost:82/'))
+gql({ transaction }).listen(82, () => console.log(`${process.env.USE_NEW_API === 'true' ? 'New' : 'Old'} GQL running on http://localhost:82/`))

60f0b41048deed7f9b12013a767f3887985171bf
Nick Collings <nick@collingo.com>
2017-12-15 10:20:00 +0000

    Update plural relation tests to act as better documentation

--- src/test/lib/define-plural-relation.js
+++ src/test/lib/define-plural-relation.js
@@ -11,26 +11,21 @@ describe('definePluralRelation', () => {
 
   it('should throw if no type is given', () => {
     expect(() => definePluralRelation({
-      parentType: 'Query',
-      name: 'jobs'
+      parentType: 'Query'
     })).to.throw('definePluralRelation requires a type')
   })
 
   it('should return an object', () => {
     expect(definePluralRelation({
       parentType: 'Query',
-      name: 'jobs',
-      type: 'Job',
-      collection: 'jobs'
+      type: 'Job'
     })).to.be.an('object')
   })
 
   it('should return the typeDefs', () => {
     expect(definePluralRelation({
       parentType: 'Query',
-      name: 'jobs',
-      type: 'Job',
-      collection: 'jobs'
+      type: 'Job'
     })).to.have.property('typeDefs').to.equal(`
       extend type Query {
         jobs: [Job!]!
@@ -38,12 +33,10 @@ describe('definePluralRelation', () => {
     `)
   })
 
-  it('should return resolver patch for Query.jobs', () => {
+  it('should return resolver for Company.jobs', () => {
     expect(definePluralRelation({
       parentType: 'Query',
-      name: 'jobs',
-      type: 'Job',
-      collection: 'jobs'
+      type: 'Job'
     }))
     .to.have.property('resolvers')
     .to.have.property('Query')
@@ -51,34 +44,38 @@ describe('definePluralRelation', () => {
   })
 
   describe('the resolver', () => {
-    const resolver = definePluralRelation({
-      parentType: 'Query',
-      name: 'jobs',
-      type: 'Job',
-      collection: 'jobs'
-    }).resolvers.Query.jobs
+    let resolver
+
+    beforeEach(() => {
+      resolver = definePluralRelation({
+        parentType: 'Query',
+        type: 'Job'
+      }).resolvers.Query.jobs
+    })
 
     it('should be a function', () => {
       expect(resolver).to.be.a('function')
     })
 
     it('should return the result of a store.readAll call', () => {
+      const fakeStore = {
+        readAll: () => 'all_the_jobs'
+      }
       const fakeContext = {
         transaction: (action) => {
-          return action({
-            readAll: () => 'all_the_jobs'
-          }, {})
+          return action(fakeStore, {})
         }
       }
       expect(resolver(null, null, fakeContext)).to.equal('all_the_jobs')
     })
 
-    it('should store.readAll with the collection type', () => {
+    it('should call store.readAll with the collection type', () => {
+      const fakeStore = {
+        readAll: ({ type }) => type
+      }
       const fakeContext = {
         transaction: (action) => {
-          return action({
-            readAll: ({ type }) => type
-          }, {
+          return action(fakeStore, {
             collection: 'jobs'
           })
         }
@@ -87,39 +84,93 @@ describe('definePluralRelation', () => {
     })
   })
 
-  describe('when the name is missing', () => {
-    it('should generate a name based on the type', () => {
+  // optional parameters
+
+  describe('when the name is passed in', () => {
+    it('should override the name', () => {
       expect(definePluralRelation({
         parentType: 'Query',
-        type: 'SomeLongObscureType',
-        collection: 'jobs'
+        type: 'Job',
+        name: 'aDifferentName'
       })).to.have.property('typeDefs').to.equal(`
       extend type Query {
-        someLongObscureTypes: [SomeLongObscureType!]!
+        aDifferentName: [Job!]!
       }
     `)
     })
   })
 
-  describe('when the collection is missing the resolver', () => {
-    it('should call store.readOne with a collection that is a pluralisation of the type', () => {
-      const resolver = definePluralRelation({
-        parentType: 'Query',
-        name: 'job',
-        type: 'SomeLongObscureType'
-      }).resolvers.Query.job
-      const type = 'someLongObscureTypes'
-      const id = 'jobId'
-      const args = { id }
-      const store = {
-        readAll: ({ type }) => type
-      }
-      const fakeContext = {
-        transaction: (action, params) => {
-          return action(store, params)
+  describe('when the collection is passed in', () => {
+    describe('the resolver', () => {
+      it('should override the type passed to store.readAll', () => {
+        const resolver = definePluralRelation({
+          parentType: 'Query',
+          type: 'Job',
+          collection: 'aDifferentCollection'
+        }).resolvers.Query.jobs
+        const store = {
+          readAll: ({ type }) => type
         }
-      }
-      expect(resolver(null, args, fakeContext)).to.deep.equal(type)
+        const fakeContext = {
+          transaction: (action, params) => {
+            return action(store, params)
+          }
+        }
+        expect(resolver(null, null, fakeContext)).to.deep.equal('aDifferentCollection')
+      })
+    })
+  })
+
+  // Sub entity relations (not root i.e. Query/Mutation)
+
+  describe('when the parent is defined', () => {
+    describe('the resolver', () => {
+      it('should call store.readOne with filters based on parent.id', () => {
+        const resolver = definePluralRelation({
+          parentType: 'Company',
+          type: 'Job'
+        }).resolvers.Company.jobs
+        const parent = {
+          id: 'company1'
+        }
+        const store = {
+          readAll: ({ filters }) => filters
+        }
+        const fakeContext = {
+          transaction: (action, params) => {
+            return action(store, params)
+          }
+        }
+        expect(resolver(parent, null, fakeContext)).to.deep.equal({
+          company: 'company1'
+        })
+      })
+    })
+
+    describe('when parentName is passed in', () => {
+      describe('the resolver', () => {
+        it('should override key in filters for parent.id', () => {
+          const resolver = definePluralRelation({
+            parentType: 'Company',
+            parentName: 'aDifferentName',
+            type: 'Job'
+          }).resolvers.Company.jobs
+          const parent = {
+            id: 'company1'
+          }
+          const store = {
+            readAll: ({ filters }) => filters
+          }
+          const fakeContext = {
+            transaction: (action, params) => {
+              return action(store, params)
+            }
+          }
+          expect(resolver(parent, null, fakeContext)).to.deep.equal({
+            aDifferentName: 'company1'
+          })
+        })
+      })
     })
   })
 })

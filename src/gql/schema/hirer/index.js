ed158c5d302ab600367dae20d367100e9d9bbf59
Nick Collings <nick@collingo.com>
2017-12-18 17:26:19 +0000

    Add Hirer.onboarded property and Hirer.setOnboarded custom resolver

--- src/gql/schema/hirer/index.js
+++ src/gql/schema/hirer/index.js
@@ -4,6 +4,7 @@ module.exports = {
       id: ID!
       created: DateTime!
       modified: DateTime!
+      onboarded: Boolean!
     }
 
     input HirerFilterInput {
--- src/test/schema/hirer/properties.js
+++ src/test/schema/hirer/properties.js
@@ -9,6 +9,7 @@ const TYPE = 'Hirer'
 const TYPE_PLURAL = 'hirers'
 const DUMMY_ID = '123'
 const DUMMY_DATETIME = '2000-01-17T02:51:58.000+00:00'
+const DUMMY_BOOLEAN = true
 expectPropertyReceivesValue = expectPropertyReceivesValue(schema, TYPE, TYPE_PLURAL)
 expectPropertyIsRequired = expectPropertyIsRequired(schema, TYPE, TYPE_PLURAL)
 
@@ -17,10 +18,12 @@ describe('Hirer properties', () => {
     await expectPropertyReceivesValue('id', DUMMY_ID)
     await expectPropertyReceivesValue('created', DUMMY_DATETIME)
     await expectPropertyReceivesValue('modified', DUMMY_DATETIME)
+    await expectPropertyReceivesValue('onboarded', DUMMY_BOOLEAN)
   })
   it('should have the following required properties', async () => {
     await expectPropertyIsRequired('id')
     await expectPropertyIsRequired('created')
     await expectPropertyIsRequired('modified')
+    await expectPropertyIsRequired('onboarded')
   })
 })

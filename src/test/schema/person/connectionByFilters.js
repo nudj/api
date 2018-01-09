6cdfa434bb0251439e5165bf9b30e3f30be8f1e5
Richard Palmer <rich@rdjpalmer.com>
2018-01-09 17:34:47 +0000

    Add connectionByFilters resolver

---
+++ src/test/schema/person/connectionByFilters.js
@@ -0,0 +1,50 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const expect = chai.expect
+
+const schema = require('../../../gql/schema')
+const { executeQueryOnDbUsingSchema } = require('../../helpers')
+
+describe('Person.connectionByFilters', () => {
+  const operation = `
+    query testQuery ($userId: ID!, $connectionId: ID!) {
+      person (id: $userId) {
+        connection: connectionByFilters(filters: {id: $connectionId}) {
+          id
+        }
+      }
+    }
+  `
+  it('should update the task and return the result', async () => {
+    const db = {
+      people: [
+        {
+          id: 'person1'
+        }
+      ],
+      connections: [
+        {
+          from: 'person1',
+          id: 'connection1'
+        }
+      ]
+    }
+
+    const variables = {
+      userId: 'person1',
+      connectionId: 'connection1'
+    }
+
+    return executeQueryOnDbUsingSchema({ operation, db, schema, variables }).then(result => {
+      return expect(result).to.deep.equal({
+        data: {
+          person: {
+            connection: {
+              id: 'connection1'
+            }
+          }
+        }
+      })
+    })
+  })
+})

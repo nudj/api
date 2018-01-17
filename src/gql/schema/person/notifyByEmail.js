0182be182c259fe2eed007d304ac4b91604dd694
Richard Palmer <rich@rdjpalmer.com>
2018-01-17 14:04:06 +0000

    Add Person.notifyByEmail resolver (#199)
    * Add Person.notifyByEmail resolver
    
    * Upgrade @nudj/library@4.2.0

---
+++ src/gql/schema/person/notifyByEmail.js
@@ -0,0 +1,37 @@
+const { renderSimpleTemplate } = require('@nudj/library')
+
+const { handleErrors } = require('../../lib')
+const mailer = require('../../lib/mailer')
+
+module.exports = {
+  typeDefs: `
+    extend type Person {
+      notifyByEmail(
+        subject: String!,
+        body: String!,
+      ): Status
+    }
+  `,
+  resolvers: {
+    Person: {
+      notifyByEmail: handleErrors(async (person, args) => {
+        return mailer.send({
+          from: 'hello@nudj.co',
+          to: person.email,
+          subject: renderSimpleTemplate({
+            template: args.subject,
+            data: person,
+            brify: () => '',
+            splitter: 'somerandomstring'
+          })[0].join(''),
+          html: renderSimpleTemplate({
+            template: args.body,
+            data: person,
+            brify: () => '',
+            splitter: 'somerandomstring'
+          })[0].join('')
+        })
+      })
+    }
+  }
+}

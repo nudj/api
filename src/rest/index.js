bdc7e9ca0c743c7d79aaf21ce06f2fb634e3c372
Timothy Robinson <timmyrobby@gmail.com>
2018-01-25 10:49:15 +0000

    Remove unnecessary invokation of parseQueryBooleans

--- src/rest/index.js
+++ src/rest/index.js
@@ -7,6 +7,6 @@ const server = restify.createServer()
 server.use(morgan('combined'))
 server.use(restify.bodyParser())
 server.use(restify.queryParser())
-server.use(parseQueryBooleans())
+server.use(parseQueryBooleans)
 routes(server)
 module.exports = server

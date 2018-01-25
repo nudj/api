e1e87e679a942aa7c38173287e48f99ded063d5d
Timothy Robinson <timmyrobby@gmail.com>
2018-01-25 09:30:54 +0000

    Add boolean parser to rest api

---
+++ src/lib/parse-query-booleans.js
@@ -0,0 +1,16 @@
+const mapValues = require('lodash/mapValues')
+
+const booleanParser = (obj) => {
+  if (obj === 'true') return true
+  if (obj === 'false') return false
+  if (obj.constructor === Object) return mapValues(obj, booleanParser)
+  if (Array.isArray(obj)) return obj.map(value => booleanParser(value))
+  return obj
+}
+
+module.exports = () => {
+  return (req, res, next) => {
+    req.query = mapValues(req.query, booleanParser)
+    next()
+  }
+}

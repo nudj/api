c7334e61bed129095594746434d7576922249fab
Timothy Robinson <timmyrobby@gmail.com>
2018-02-01 11:47:04 +0000

    Remove unused GOOGLE_CLIENT_CALLBACK

--- src/gql/lib/google/auth-client.js
+++ src/gql/lib/google/auth-client.js
@@ -2,8 +2,7 @@ const { auth } = require('googleapis')
 
 const authClient = new auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_AUTH_CALLBACK
+  process.env.GOOGLE_CLIENT_SECRET
 )
 
 module.exports = authClient

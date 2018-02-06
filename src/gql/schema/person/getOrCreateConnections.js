5b275fcecc381b5bcf6762203361d6c945563bf8
Nick Collings <nick@collingo.com>
2018-02-06 12:37:11 +0000

    Add return of nested connection/employment to save fetching children

--- src/gql/schema/person/getOrCreateConnections.js
+++ src/gql/schema/person/getOrCreateConnections.js
@@ -27,41 +27,69 @@ module.exports = {
             const omit = require('lodash/omit')
             const pick = require('lodash/pick')
             const { from, to, source } = params
-            return Promise.all([
-              to.title && store.readOneOrCreate({
-                type: 'roles',
-                filters: { name: to.title },
-                data: { name: to.title }
-              }),
-              to.company && store.readOneOrCreate({
-                type: 'companies',
-                filters: { name: to.company },
-                data: { name: to.company }
-              }),
-              store.readOneOrCreate({
-                type: 'people',
-                filters: { email: to.email },
-                data: pick(to, ['email'])
-              })
-            ])
-            .then(([
-              role,
-              company,
-              person
-            ]) => {
-              return store.readOneOrCreate({
+
+            return store.readOne({
+              type: 'people',
+              filters: { email: to.email }
+            })
+            .then(person => Promise.all([
+              person,
+              person && store.readOne({
                 type: 'connections',
                 filters: {
                   from,
                   person: person.id
-                },
-                data: Object.assign({}, omit(to, ['email', 'title']), {
-                  from,
-                  source: source.id,
-                  role: role && role.id,
-                  company: company && company.id,
-                  person: person.id
+                }
+              })
+            ]))
+            .then(([
+              person,
+              connection
+            ]) => {
+              if (connection) return Object.assign({}, connection, { person })
+              return Promise.all([
+                person || store.create({
+                  type: 'people',
+                  data: pick(to, ['email'])
+                }),
+                store.readOneOrCreate({
+                  type: 'sources',
+                  filters: { name: source.name },
+                  data: source
+                }),
+                to.title && store.readOneOrCreate({
+                  type: 'roles',
+                  filters: { name: to.title },
+                  data: { name: to.title }
+                }),
+                to.company && store.readOneOrCreate({
+                  type: 'companies',
+                  filters: { name: to.company },
+                  data: { name: to.company, client: false }
                 })
+              ])
+              .then(([
+                person,
+                source,
+                role,
+                company
+              ]) => {
+                return store.create({
+                  type: 'connections',
+                  data: Object.assign({}, omit(to, ['email', 'title']), {
+                    from,
+                    source: source.id,
+                    role: role ? role.id : null,
+                    company: company ? company.id : null,
+                    person: person.id
+                  })
+                })
+                .then(connection => Object.assign({}, connection, {
+                  source,
+                  role,
+                  company,
+                  person
+                }))
               })
             })
           }, {

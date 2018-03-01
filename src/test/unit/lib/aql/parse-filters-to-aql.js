6f782faac806897bb95cbf7cfb500b4559f5638d
Richard Palmer <rich@rdjpalmer.com>
2018-03-01 10:42:47 +0000

    Fix missing tests
    `test/lib` => `test/unit/lib` to ensure they run with `test` and `tdd`
    commands

--- src/test/unit/lib/aql/parse-filters-to-aql.js
+++ src/test/unit/lib/aql/parse-filters-to-aql.js
@@ -3,7 +3,7 @@ const chai = require('chai')
 const dedent = require('dedent')
 const expect = chai.expect
 
-const { parseFiltersToAql } = require('../../../gql/lib/aql')
+const { parseFiltersToAql } = require('../../../../gql/lib/aql')
 
 describe('parseFiltersToAql', () => {
   it('formats a basic filters object to an AQL string', () => {

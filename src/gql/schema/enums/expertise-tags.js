743cc1ce2eb0eec3b0717c3958a509e55f0d73bb
Timothy Robinson <timmyrobby@gmail.com>
2018-03-16 10:25:11 +0000

    Add expertise tags enum

---
+++ src/gql/schema/enums/expertise-tags.js
@@ -0,0 +1,29 @@
+const { rootEnum } = require('../../lib')
+
+module.exports = rootEnum({
+  name: 'Expertise',
+  values: [
+    'ceo',
+    'communications',
+    'consulting',
+    'customerService',
+    'education',
+    'engineering',
+    'finance',
+    'founder',
+    'healthProfessional',
+    'humanResources',
+    'informationTechnology',
+    'legal',
+    'marketing',
+    'operations',
+    'owner',
+    'president',
+    'product',
+    'publicRelations',
+    'realEstate',
+    'recruiting',
+    'research',
+    'sales'
+  ]
+})

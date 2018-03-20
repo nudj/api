7a7c2ed494dc0f55427a33f39511afd317991cfc
Timothy Robinson <timmyrobby@gmail.com>
2018-03-20 17:55:43 +0000

    Update migration to add entityTags to jobs

--- src/migrations/00006-tags-collections/index.js
+++ src/migrations/00006-tags-collections/index.js
@@ -1,72 +1,82 @@
 async function up ({ db, step }) {
-  await step('Create entityTags collection', async () => {
-    try {
-      const collection = db.collection('entityTags')
-      await collection.create()
-    } catch (error) {
-      if (error.message !== 'duplicate name: duplicate name') {
-        throw error
+  await step('Create new collections', async () => {
+    await Promise.all([
+      'entityTags',
+      'tags'
+    ].map(async name => {
+      try {
+        const collection = db.collection(name)
+        await collection.create()
+      } catch (error) {
+        if (error.message !== 'duplicate name: duplicate name') {
+          throw error
+        }
       }
-    }
+    }))
   })
 
-  await step('Create tags collection', async () => {
-    try {
-      const collection = db.collection('tags')
-      await collection.create()
-    } catch (error) {
-      if (error.message !== 'duplicate name: duplicate name') {
-        throw error
-      }
-    }
-  })
-
-  await step('Add empty `tags` arrays to surveyQuestions', async () => {
+  await step('Add empty `entityTags` arrays to tagged collections', async () => {
     const surveyQuestionsCollection = db.collection('surveyQuestions')
+    const jobsCollection = db.collection('jobs')
     const allSurveyQuestions = await surveyQuestionsCollection.all()
-    await allSurveyQuestions.each(async question => {
-      if (!question.tags) {
-        return surveyQuestionsCollection.update(question, {
-          tags: []
-        })
-      }
-    })
+    const allJobs = await jobsCollection.all()
+    await Promise.all([
+      allSurveyQuestions.each(async question => {
+        if (!question.entityTags) {
+          return surveyQuestionsCollection.update(question, {
+            entityTags: []
+          })
+        }
+      }),
+      allJobs.each(async job => {
+        if (!job.entityTags) {
+          return jobsCollection.update(job, {
+            entityTags: []
+          })
+        }
+      })
+    ])
   })
 }
 
 async function down ({ db, step }) {
-  await step('Remove entityTags collection', async () => {
-    try {
-      const collection = db.collection('entityTags')
-      await collection.drop()
-    } catch (error) {
-      if (error.message !== `unknown collection 'entityTags'`) {
-        throw error
+  await step('Remove new collections', async () => {
+    await Promise.all([
+      'entityTags',
+      'tags'
+    ].map(async name => {
+      try {
+        const collection = db.collection(name)
+        await collection.drop()
+      } catch (error) {
+        if (error.message !== `unknown collection '${name}'`) {
+          throw error
+        }
       }
-    }
+    }))
   })
 
-  await step('Remove tags collection', async () => {
-    try {
-      const collection = db.collection('tags')
-      await collection.drop()
-    } catch (error) {
-      if (error.message !== `unknown collection 'tags'`) {
-        throw error
-      }
-    }
-  })
-
-  await step('Remove empty `tags` arrays from surveyQuestions', async () => {
+  await step('Remove empty `entityTags` arrays from tagged collections', async () => {
     const surveyQuestionsCollection = db.collection('surveyQuestions')
+    const jobsCollection = db.collection('jobs')
     const allSurveyQuestions = await surveyQuestionsCollection.all()
-    await allSurveyQuestions.each(async question => {
-      if (question.tags && question.tags.length === 0) {
-        return surveyQuestionsCollection.update(question, {
-          tags: null
-        }, { keepNull: false })
-      }
-    })
+    const allJobs = await jobsCollection.all()
+    await Promise.all([
+      allSurveyQuestions.each(async question => {
+        if (question.entityTags && !question.entityTags.length) {
+          return surveyQuestionsCollection.update(question, {
+            entityTags: null
+          }, { keepNull: false })
+        }
+      }),
+      allJobs.each(async job => {
+        if (job.entityTags && !job.entityTags.length) {
+          return jobsCollection.update(job, {
+            entityTags: null
+          }, { keepNull: false })
+        }
+      })
+    ])
   })
 }
 

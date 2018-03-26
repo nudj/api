cf48b57ff11ba5ec63dd58e4c349e38d07071ff4
Timothy Robinson <timmyrobby@gmail.com>
2018-03-26 18:52:01 +0100

    Add CompanyUpdateInput to schema

--- src/gql/schema/company/index.js
+++ src/gql/schema/company/index.js
@@ -37,6 +37,23 @@ module.exports = {
       client: Boolean!
     }
 
+    input CompanyUpdateInput {
+      name: String
+      slug: String
+      description: String
+      mission: String
+      facebook: String
+      industry: String
+      linkedin: String
+      location: String
+      logo: String
+      size: String
+      twitter: String
+      url: String
+      onboarded: Boolean
+      client: Boolean
+    }
+
     input CompanyFilterInput {
       id: ID
       slug: String

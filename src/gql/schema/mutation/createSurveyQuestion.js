a466611260b5f406e22dbb49fed59e2c486bc1eb
Timothy Robinson <timmyrobby@gmail.com>
2018-03-28 14:39:49 +0100

    Prevent entityTags from being added to surveyQuestion in createSurveyQuestion resolver

--- src/gql/schema/mutation/createSurveyQuestion.js
+++ src/gql/schema/mutation/createSurveyQuestion.js
@@ -49,7 +49,7 @@ module.exports = {
           })
         }))
 
-        const surveyQuestionEntityTags = await Promise.all(surveyTags.map(tag => {
+        await Promise.all(surveyTags.map(tag => {
           return context.store.create({
             type: 'entityTags',
             data: {
@@ -62,13 +62,7 @@ module.exports = {
           })
         }))
 
-        return context.store.update({
-          type: 'surveyQuestions',
-          id: surveyQuestion.id,
-          data: {
-            entityTags: surveyQuestionEntityTags.map(tag => tag.id)
-          }
-        })
+        return surveyQuestion
       })
     }
   }

2dc24a06a6cb1f10064ab2192cc8bec92e0e0bc2
Timothy Robinson <timmyrobby@gmail.com>
2018-04-17 09:28:09 +0100

    Add common lib to fetch connections roleTagMap

---
+++ src/gql/lib/helpers/fetch-role-tag-maps.js
@@ -0,0 +1,30 @@
+const fetchRoleToTagsMap = async (context) => {
+  const [
+    roleTags,
+    roleRelatedTags
+  ] = await Promise.all([
+    await context.store.readAll({
+      type: 'roleTags'
+    }),
+    await context.store.readAll({
+      type: 'tags'
+    })
+  ])
+
+  const rolesToRoleTagsMap = roleTags.reduce((map, roleTag) => {
+    map[roleTag.role] = (map[roleTag.role] || []).concat(roleTag.tag)
+    return map
+  }, {})
+
+  const roleTagsToTagsMap = roleRelatedTags.reduce((map, tag) => {
+    map[tag.id] = map[tag.id] || tag
+    return map
+  }, {})
+
+  return Object.keys(rolesToRoleTagsMap).reduce((map, roleId) => {
+    map[roleId] = (rolesToRoleTagsMap[roleId] || []).map(tagId => roleTagsToTagsMap[tagId])
+    return map
+  }, {})
+}
+
+module.exports = fetchRoleToTagsMap

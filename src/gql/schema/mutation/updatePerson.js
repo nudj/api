55b99afdfb12a149a165ee1dfb53f3e30f3791b3
Timothy Robinson <timmyrobby@gmail.com>
2018-04-24 15:28:47 +0100

    Update updatePerson resolver to update employment and company relations

--- src/gql/schema/mutation/updatePerson.js
+++ src/gql/schema/mutation/updatePerson.js
@@ -1,4 +1,7 @@
+const omit = require('lodash/omit')
 const { handleErrors } = require('../../lib')
+const { values: dataSources } = require('../enums/data-sources')
+const getMakeUniqueCompanySlug = require('../../lib/helpers/make-unique-company-slug')
 
 module.exports = {
   typeDefs: `
@@ -8,12 +11,86 @@ module.exports = {
   `,
   resolvers: {
     Mutation: {
-      updatePerson: handleErrors((person, args, context) => {
+      updatePerson: handleErrors(async (root, args, context) => {
         const { id, data } = args
+        const { company: companyName } = data
+
+        if (!companyName) {
+          return context.store.update({
+            type: 'people',
+            id,
+            data: omit(data, ['company'])
+          })
+        }
+
+        // Fetch company by name
+        let company = await context.store.readOne({
+          type: 'companies',
+          filters: { name: companyName }
+        })
+
+        // If company does not exist, create it
+        if (!company) {
+          const allCompanies = await context.store.readAll({
+            type: 'companies'
+          })
+          const makeSlug = getMakeUniqueCompanySlug(allCompanies)
+          company = await context.store.create({
+            type: 'companies',
+            data: {
+              name: companyName,
+              onboarded: false,
+              client: false,
+              slug: makeSlug({ name: companyName })
+            }
+          })
+        }
+
+        // Fetch current employment for person
+        const currentEmployment = await context.store.readOne({
+          type: 'employments',
+          filters: {
+            person: id,
+            current: true
+          }
+        })
+
+        // If a current employment doesn't exist, create one. Otherwise, compare that
+        // current employment to the PersonUpdateInput company.
+        if (!currentEmployment) {
+          await context.store.create({
+            type: 'employments',
+            data: {
+              person: id,
+              current: true,
+              company: company.id,
+              source: dataSources.NUDJ
+            }
+          })
+        } else if (currentEmployment && currentEmployment.company !== company.id) {
+          // If the current employment is different from the new data
+          await context.store.update({
+            type: 'employments',
+            id: currentEmployment.id,
+            data: {
+              current: false
+            }
+          })
+          await context.store.create({
+            type: 'employments',
+            data: {
+              person: id,
+              current: true,
+              company: company.id,
+              source: dataSources.NUDJ
+            }
+          })
+        }
+
         return context.store.update({
           type: 'people',
           id,
-          data
+          data: omit(data, ['company'])
         })
       })
     }
--- src/test/unit/schema/mutation/updatePerson.js
+++ src/test/unit/schema/mutation/updatePerson.js
@@ -6,57 +6,489 @@ const schema = require('../../../../gql/schema')
 const { executeQueryOnDbUsingSchema } = require('../../helpers')
 
 describe('Mutation.updatePerson', () => {
-  it("should update the person's data", () => {
-    const db = {
-      people: [
-        {
-          id: 'person1',
-          email: '',
-          firstName: '',
-          lastName: '',
-          url: '',
-          emailPreference: ''
-        }
-      ]
-    }
-
-    const operation = `
-      mutation SetEmailPreference ($emailPreference: EmailPreference) {
-        updatePerson(
-          id: "person1",
-          data: {
-            email: "test@test.tld",
-            firstName: "Tester",
-            lastName: "McTestFace",
-            url: "lol.tld",
-            emailPreference: $emailPreference
+  describe('when a company name is not specified', () => {
+    it("should update the person's data", () => {
+      const db = {
+        people: [
+          {
+            id: 'person1',
+            email: '',
+            firstName: '',
+            lastName: '',
+            url: '',
+            emailPreference: ''
           }
-        ) {
-          id
-          email
-          firstName
-          lastName
-          url
-          emailPreference
-        }
+        ],
+        companies: [],
+        employments: []
       }
-    `
 
-    const variables = { emailPreference: 'GOOGLE' }
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
 
-    return executeQueryOnDbUsingSchema({
-      operation,
-      variables,
-      db,
-      schema
-    }).then(() => {
-      return expect(db.people[0]).to.deep.equal({
-        id: 'person1',
-        email: 'test@test.tld',
-        firstName: 'Tester',
-        lastName: 'McTestFace',
-        url: 'lol.tld',
-        emailPreference: 'GOOGLE'
+      const variables = { emailPreference: 'GOOGLE' }
+
+      return executeQueryOnDbUsingSchema({
+        operation,
+        variables,
+        db,
+        schema
+      }).then(() => {
+        return expect(db.people[0]).to.deep.equal({
+          id: 'person1',
+          email: 'test@test.tld',
+          firstName: 'Tester',
+          lastName: 'McTestFace',
+          url: 'lol.tld',
+          emailPreference: 'GOOGLE'
+        })
+      })
+    })
+  })
+
+  describe('when a company name is specified', () => {
+    describe('when the company exists', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              company: "nudj",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should create an employment', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.employments[0]).to.deep.equal({
+            company: 'company1',
+            current: true,
+            id: 'employment1',
+            person: 'person1',
+            source: 'NUDJ'
+          })
+        })
+      })
+
+      it('should not create a company', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.companies.length).to.equal(1)
+          return expect(db.companies[0]).to.deep.equal({
+            id: 'company1',
+            name: 'nudj'
+          })
+        })
+      })
+    })
+
+    describe('when the company and employment exists', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              company: "nudj",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: [
+            {
+              id: 'employment1',
+              company: 'company1',
+              person: 'person1',
+              current: true
+            }
+          ]
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should not create an employment', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: [
+            {
+              id: 'employment1',
+              company: 'company1',
+              person: 'person1',
+              current: true
+            }
+          ]
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.employments.length).to.equal(1)
+          return expect(db.employments[0]).to.deep.equal({
+            id: 'employment1',
+            company: 'company1',
+            person: 'person1',
+            current: true
+          })
+        })
+      })
+
+      it('should not create a company', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [
+            {
+              id: 'company1',
+              name: 'nudj'
+            }
+          ],
+          employments: [
+            {
+              id: 'employment1',
+              company: 'company1',
+              person: 'person1',
+              current: true
+            }
+          ]
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.companies.length).to.equal(1)
+          return expect(db.companies[0]).to.deep.equal({
+            id: 'company1',
+            name: 'nudj'
+          })
+        })
+      })
+    })
+
+    describe('when the company does not exist', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              company: "nudj",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should create an employment', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.employments[0]).to.deep.equal({
+            company: 'company1',
+            current: true,
+            id: 'employment1',
+            person: 'person1',
+            source: 'NUDJ'
+          })
+        })
+      })
+
+      it('should create a company', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              company: 'nudj',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.companies.length).to.equal(1)
+          return expect(db.companies[0]).to.deep.equal({
+            id: 'company1',
+            client: false,
+            onboarded: false,
+            slug: 'nudj',
+            name: 'nudj'
+          })
+        })
       })
     })
   })

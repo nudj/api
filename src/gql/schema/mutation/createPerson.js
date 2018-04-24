d0f5ec3a7801b552a8caa216196b03bb141b0129
Timothy Robinson <timmyrobby@gmail.com>
2018-04-24 15:28:09 +0100

    Update createPerson resolver to update emplyoment and company relations

--- src/gql/schema/mutation/createPerson.js
+++ src/gql/schema/mutation/createPerson.js
@@ -1,4 +1,7 @@
+const omit = require('lodash/omit')
 const { handleErrors } = require('../../lib')
+const { values: dataSources } = require('../enums/data-sources')
+const getMakeUniqueCompanySlug = require('../../lib/helpers/make-unique-company-slug')
 
 module.exports = {
   typeDefs: `
@@ -8,11 +11,44 @@ module.exports = {
   `,
   resolvers: {
     Mutation: {
-      createPerson: handleErrors((root, args, context) => {
-        return context.store.create({
-          type: 'people',
-          data: args.input
+      createPerson: handleErrors(async (root, args, context) => {
+        const { company: companyName } = args.input
+
+        let company = await context.store.readOne({
+          type: 'companies',
+          filters: { name: companyName }
         })
+
+        if (!company) {
+          const allCompanies = await context.store.readAll({
+            type: 'companies'
+          })
+          const makeSlug = getMakeUniqueCompanySlug(allCompanies)
+          company = await context.store.create({
+            type: 'companies',
+            data: {
+              name: companyName,
+              slug: makeSlug({ name: companyName })
+            }
+          })
+        }
+
+        const person = await context.store.create({
+          type: 'people',
+          data: omit(args.input, ['company'])
+        })
+
+        await context.store.create({
+          type: 'employments',
+          data: {
+            person: person.id,
+            company: company.id,
+            current: true,
+            source: dataSources.NUDJ
+          }
+        })
+
+        return person
       })
     }
   }

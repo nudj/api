828a0882258ebf6ba06d57045ce024911fca7e78
Timothy Robinson <timmyrobby@gmail.com>
2018-04-24 19:03:27 +0100

    Update updatePerson resolver to handle role name updates

--- src/test/unit/schema/mutation/updatePerson.js
+++ src/test/unit/schema/mutation/updatePerson.js
@@ -6,8 +6,8 @@ const schema = require('../../../../gql/schema')
 const { executeQueryOnDbUsingSchema } = require('../../helpers')
 
 describe('Mutation.updatePerson', () => {
-  describe('when a company name is not specified', () => {
-    it("should update the person's data", () => {
+  describe('when a company name or role name is not specified', () => {
+    it('should update the person\'s data', () => {
       const db = {
         people: [
           {
@@ -487,6 +487,445 @@ describe('Mutation.updatePerson', () => {
             onboarded: false,
             slug: 'nudj',
             name: 'nudj'
+          })
+        })
+      })
+    })
+  })
+
+  describe('when a role name is specified', () => {
+    describe('when the role exists', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              role: "worker",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          personRoles: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should create a personRole', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              role: 'worker',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          personRoles: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.personRoles[0]).to.deep.equal({
+            id: 'personRole1',
+            current: true,
+            role: 'role1',
+            person: 'person1',
+            source: 'NUDJ'
+          })
+        })
+      })
+
+      it('should not create a role', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              role: 'worker',
+              emailPreference: ''
+            }
+          ],
+          companies: [],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          personRoles: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.roles.length).to.equal(1)
+          return expect(db.roles[0]).to.deep.equal({
+            id: 'role1',
+            name: 'worker'
+          })
+        })
+      })
+    })
+
+    describe('when the role and personRole exists', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              role: "worker",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [
+            {
+              id: 'personRole1',
+              role: 'role1',
+              person: 'person1',
+              current: true
+            }
+          ],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should not create a personRole', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [
+            {
+              id: 'personRole1',
+              role: 'role1',
+              person: 'person1',
+              current: true
+            }
+          ],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.personRoles.length).to.equal(1)
+          return expect(db.personRoles[0]).to.deep.equal({
+            id: 'personRole1',
+            role: 'role1',
+            person: 'person1',
+            current: true
+          })
+        })
+      })
+
+      it('should not create a role', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [
+            {
+              id: 'personRole1',
+              role: 'role1',
+              person: 'person1',
+              current: true
+            }
+          ],
+          roles: [
+            {
+              id: 'role1',
+              name: 'worker'
+            }
+          ],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.roles.length).to.equal(1)
+          return expect(db.roles[0]).to.deep.equal({
+            id: 'role1',
+            name: 'worker'
+          })
+        })
+      })
+    })
+
+    describe('when the role does not exist', () => {
+      const operation = `
+        mutation SetEmailPreference ($emailPreference: EmailPreference) {
+          updatePerson(
+            id: "person1",
+            data: {
+              email: "test@test.tld",
+              firstName: "Tester",
+              lastName: "McTestFace",
+              url: "lol.tld",
+              role: "worker",
+              emailPreference: $emailPreference
+            }
+          ) {
+            id
+            email
+            firstName
+            lastName
+            url
+            emailPreference
+          }
+        }
+      `
+
+      const variables = { emailPreference: 'GOOGLE' }
+
+      it('should update the person\'s data', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [],
+          roles: [],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.people[0]).to.deep.equal({
+            id: 'person1',
+            email: 'test@test.tld',
+            firstName: 'Tester',
+            lastName: 'McTestFace',
+            url: 'lol.tld',
+            emailPreference: 'GOOGLE'
+          })
+        })
+      })
+
+      it('should create a personRole', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [],
+          roles: [],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          return expect(db.personRoles[0]).to.deep.equal({
+            id: 'personRole1',
+            role: 'role1',
+            current: true,
+            person: 'person1',
+            source: 'NUDJ'
+          })
+        })
+      })
+
+      it('should create a role', () => {
+        const db = {
+          people: [
+            {
+              id: 'person1',
+              email: '',
+              firstName: '',
+              lastName: '',
+              url: '',
+              emailPreference: ''
+            }
+          ],
+          personRoles: [],
+          roles: [],
+          companies: [],
+          employments: []
+        }
+
+        return executeQueryOnDbUsingSchema({
+          operation,
+          variables,
+          db,
+          schema
+        }).then(() => {
+          expect(db.roles.length).to.equal(1)
+          return expect(db.roles[0]).to.deep.equal({
+            id: 'role1',
+            name: 'worker'
           })
         })
       })

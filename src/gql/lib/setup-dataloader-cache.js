a7bc230347819d15816dcb66563f83955bc13f31
Timothy Robinson <timmyrobby@gmail.com>
2018-04-26 13:30:24 +0100

    Setup dataloader to return results by key order

--- src/gql/lib/setup-dataloader-cache.js
+++ src/gql/lib/setup-dataloader-cache.js
@@ -1,8 +1,12 @@
+const find = require('lodash/find')
 const DataLoader = require('dataloader')
 
 module.exports = (db, cache) => type => {
   if (!cache[type]) {
-    cache[type] = new DataLoader(keys => db.collection(type).lookupByKeys(keys))
+    cache[type] = new DataLoader(async keys => {
+      const items = await db.collection(type).lookupByKeys(keys)
+      return keys.map(key => find(items, { _key: key }))
+    })
   }
   return cache[type]
 }

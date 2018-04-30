0b8085887daadcc997829c2ed662fdc5f53e6db1
Timothy Robinson <timmyrobby@gmail.com>
2018-04-30 11:14:25 +0100

    minor: Add INTERNAL_EMAIL_ADDRESS constant

--- src/gql/lib/constants.js
+++ src/gql/lib/constants.js
@@ -1,7 +1,9 @@
 const NUDJ = 'NUDJ'
 const DB_URL = `http://${process.env.DB_HOST}:${process.env.DB_PORT}`
+const INTERNAL_EMAIL_ADDRESS = 'hello@nudj.co'
 
 module.exports = {
   NUDJ,
-  DB_URL
+  DB_URL,
+  INTERNAL_EMAIL_ADDRESS
 }

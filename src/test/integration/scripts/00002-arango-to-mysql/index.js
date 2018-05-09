26e3d72713a02fe0963028009f66f01aa75cdaa7
Nick Collings <nick@collingo.com>
2018-05-09 11:32:39 +0100

    Add hirers table tests

--- src/test/integration/scripts/00002-arango-to-mysql/index.js
+++ src/test/integration/scripts/00002-arango-to-mysql/index.js
@@ -114,7 +114,7 @@ describe.only('00002 Arango to MySQL', () => {
             name: TABLE,
             data: [
               {
-                _id: 'companies/123',
+                _id: 'people/123',
                 _rev: '_WpP1l3W---',
                 _key: '123',
                 created: '2018-02-01T01:02:03.456Z',
@@ -337,6 +337,134 @@ describe.only('00002 Arango to MySQL', () => {
         expect(records[0]).to.have.property('roleDescription', null)
         expect(records[0]).to.have.property('experience', null)
         expect(records[0]).to.have.property('requirements', null)
+      })
+    })
+  })
+
+  describe('for hirers table', () => {
+    const TABLE = tableToCollection(TABLES.HIRERS)
+    const TABLE_PEOPLE = tableToCollection(TABLES.PEOPLE)
+    const TABLE_COMPANIES = tableToCollection(TABLES.COMPANIES)
+
+    afterEach(async () => {
+      await sql(TABLE).whereNot('id', '').del()
+      await sql(TABLE_PEOPLE).whereNot('id', '').del()
+      await sql(TABLE_COMPANIES).whereNot('id', '').del()
+    })
+
+    describe('with a full data set', () => {
+      beforeEach(async () => {
+        await seedRun([
+          {
+            name: TABLE_PEOPLE,
+            data: [
+              {
+                _key: 'person1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                email: 'jim@bob.com',
+                firstName: 'Jim',
+                lastName: 'Bob'
+              }
+            ]
+          },
+          {
+            name: TABLE_COMPANIES,
+            data: [
+              {
+                _key: 'company1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                name: 'Company Ltd',
+                slug: 'company-ltd'
+              }
+            ]
+          },
+          {
+            name: TABLE,
+            data: [
+              {
+                _id: 'hirers/123',
+                _rev: '_WpP1l3W---',
+                _key: '123',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                onboarded: true,
+                type: ENUMS.HIRER_TYPES.ADMIN,
+                person: 'person1',
+                company: 'company1',
+                batchSize: 100,
+                skip: 0
+              }
+            ]
+          }
+        ])
+      })
+
+      genericExpectationsForTable(TABLE)
+
+      it('should transfer all scalar properties', async () => {
+        const records = await sql.select().from(TABLE)
+        expect(records[0]).to.have.property('onboarded', 1)
+        expect(records[0]).to.have.property('type', ENUMS.HIRER_TYPES.ADMIN)
+      })
+
+      it('should remap the relations', async () => {
+        const jobs = await sql.select().from(TABLE)
+        const companies = await sql.select().from(TABLE_COMPANIES)
+        expect(jobs[0]).to.have.property('company', companies[0].id)
+      })
+    })
+
+    describe('without optional properties', () => {
+      beforeEach(async () => {
+        await seedRun([
+          {
+            name: TABLE_PEOPLE,
+            data: [
+              {
+                _key: 'person1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                email: 'jim@bob.com',
+                firstName: 'Jim',
+                lastName: 'Bob'
+              }
+            ]
+          },
+          {
+            name: TABLE_COMPANIES,
+            data: [
+              {
+                _key: 'company1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                name: 'Company Ltd',
+                slug: 'company-ltd'
+              }
+            ]
+          },
+          {
+            name: TABLE,
+            data: [
+              {
+                _id: 'hirers/123',
+                _rev: '_WpP1l3W---',
+                _key: '123',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                person: 'person1',
+                company: 'company1'
+              }
+            ]
+          }
+        ])
+      })
+
+      it('should use defaults', async () => {
+        const records = await sql.select().from(TABLE)
+        expect(records[0]).to.have.property('onboarded', 0)
+        expect(records[0]).to.have.property('type', ENUMS.HIRER_TYPES.MEMBER)
       })
     })
   })

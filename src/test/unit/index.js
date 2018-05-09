886ff840531fa1e1f2bb08b4657e6ae54f79831e
Timothy Robinson <timmyrobby@gmail.com>
2018-05-09 16:38:13 +0100

    patch: Handle unmatched requests to ignored external domains

--- src/test/unit/index.js
+++ src/test/unit/index.js
@@ -8,19 +8,32 @@ chai.use(dirtyChai)
 
 nock.disableNetConnect() // Disable all external requests for testing
 
+const ignoredDomains = [
+  'https://api.intercom.io'
+]
+
 nock.emitter.on('no match', req => {
   const url = req.url || req.uri || {}
+  const requestDomain = `${url.protocol || req.protocol}//${url.host || req.host}`
 
-  logger('info', 'Nock: No match for request:', {
-    hostname: req.hostname,
-    method: req.method,
-    href: req.href || `${protocol}//${host}${path}`,
-    protocol: url.protocol || req.protocol,
-    host: url.host || req.host,
-    path: url.pathname || url.path || req.path,
-    port: url.port || req.port,
-    hash: url.hash || req.hash,
-    query: url.query || req.query,
-    body: req.body
-  })
+  if (ignoredDomains.includes(requestDomain)) {
+    logger('info', `Nock: Ignored '${req.method}' request on ${req.hostname}`)
+  } else {
+    const protocol = url.protocol || req.protocol
+    const host = url.host || req.host
+    const path = url.pathname || url.path || req.path
+
+    logger('info', 'Nock: No match for request:', {
+      hostname: req.hostname,
+      method: req.method,
+      href: req.href || `${protocol}//${host}${path}`,
+      protocol: protocol,
+      host: host,
+      path: path,
+      port: url.port || req.port,
+      hash: url.hash || req.hash,
+      query: url.query || req.query,
+      body: req.body
+    })
+  }
 })

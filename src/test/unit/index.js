c0a3a27408e2873e6a407780abe62d6874eaa6da
Timothy Robinson <timmyrobby@gmail.com>
2018-05-09 16:36:44 +0100

    patch: Set nock to always block external requests

--- src/test/unit/index.js
+++ src/test/unit/index.js
@@ -1,12 +1,26 @@
 /* eslint-env mocha */
 require('envkey')
-let chai = require('chai')
-let dirtyChai = require('dirty-chai')
-let expect = chai.expect
+const chai = require('chai')
+const nock = require('nock')
+const dirtyChai = require('dirty-chai')
+const { logger } = require('@nudj/library')
 chai.use(dirtyChai)
 
-describe('This', function () {
-  it('should work', function () {
-    expect(true).to.be.true()
+nock.disableNetConnect() // Disable all external requests for testing
+
+nock.emitter.on('no match', req => {
+  const url = req.url || req.uri || {}
+
+  logger('info', 'Nock: No match for request:', {
+    hostname: req.hostname,
+    method: req.method,
+    href: req.href || `${protocol}//${host}${path}`,
+    protocol: url.protocol || req.protocol,
+    host: url.host || req.host,
+    path: url.pathname || url.path || req.path,
+    port: url.port || req.port,
+    hash: url.hash || req.hash,
+    query: url.query || req.query,
+    body: req.body
   })
 })

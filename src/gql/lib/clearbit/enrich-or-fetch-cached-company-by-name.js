784ce09591aa8c41b30871a58c8ccc8233af042e
Timothy Robinson <timmyrobby@gmail.com>
2018-05-09 19:44:42 +0100

    minor: Add `enrichOrFetchCachedCompanyByName` method

---
+++ src/gql/lib/clearbit/enrich-or-fetch-cached-company-by-name.js
@@ -0,0 +1,28 @@
+const omit = require('lodash/omit')
+const { logger } = require('@nudj/library')
+const enrichCompanyByName = require('./enrich-company-by-name')
+
+const enrichOrFetchCachedCompanyByName = async (companyName, context, options) => {
+  try {
+    const enrichedCompany = await context.noSQL.readOne({
+      type: 'enrichedCompanies',
+      filters: { name: companyName }
+    })
+    if (enrichedCompany) return enrichedCompany
+
+    const companyData = await enrichCompanyByName(companyName, options)
+    return companyData && context.noSQL.create({
+      type: 'enrichedCompanies',
+      data: {
+        ...omit(companyData, ['id']), // omit Clearbit ID
+        clearbitId: companyData.id,
+        name: companyName
+      }
+    })
+  } catch (error) {
+    logger('error', 'Enrichment error', error)
+    return null
+  }
+}
+
+module.exports = enrichOrFetchCachedCompanyByName
--- src/gql/lib/clearbit/index.js
+++ src/gql/lib/clearbit/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   enrichCompanyByName: require('./enrich-company-by-name'),
   enrichCompanyByDomain: require('./enrich-company-by-domain'),
+  enrichOrFetchCachedCompanyByName: require('./enrich-or-fetch-cached-company-by-name'),
   fetchDomainForCompany: require('./fetch-domain-for-company')
 }

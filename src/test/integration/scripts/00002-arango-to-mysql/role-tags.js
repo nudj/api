2328f6a3214c2a6c3af658e0f42085da27978378
Nick Collings <nick@collingo.com>
2018-05-10 17:59:52 +0100

    Add roleTags table tests

---
+++ src/test/integration/scripts/00002-arango-to-mysql/role-tags.js
@@ -0,0 +1,121 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+const {
+  db,
+  sql,
+  setupCollections,
+  populateCollections,
+  truncateCollections,
+  teardownCollections,
+  expect,
+  genericExpectationsForTable
+} = require('../../lib')
+const {
+  TABLES,
+  ENUMS
+} = require('../../../../lib/sql')
+const {
+  TABLE_ORDER,
+  tableToCollection
+} = require('../../../../scripts/00002-arango-to-mysql/helpers')
+
+const script = require('../../../../scripts/00002-arango-to-mysql')
+
+chai.use(chaiAsPromised)
+
+describe('00002 Arango to MySQL', () => {
+  async function seedRun (data) {
+    await populateCollections(db, data)
+    await script({ db, sql })
+  }
+
+  before(async () => {
+    await setupCollections(db, TABLE_ORDER.map(table => tableToCollection(table)))
+  })
+
+  afterEach(async () => {
+    await truncateCollections(db)
+  })
+
+  after(async () => {
+    await teardownCollections(db)
+  })
+
+  describe('for roleTags table', () => {
+    const COLLECTIONS = {
+      ROLE_TAGS: tableToCollection(TABLES.ROLE_TAGS),
+      ROLES: tableToCollection(TABLES.ROLES),
+      TAGS: tableToCollection(TABLES.TAGS)
+    }
+
+    afterEach(async () => {
+      await sql(TABLES.ROLE_TAGS).whereNot('id', '').del()
+      await sql(TABLES.ROLES).whereNot('id', '').del()
+      await sql(TABLES.TAGS).whereNot('id', '').del()
+    })
+
+    describe('with a full data set', () => {
+      beforeEach(async () => {
+        await seedRun([
+          {
+            name: COLLECTIONS.ROLES,
+            data: [
+              {
+                _key: 'role1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                name: 'Role name'
+              }
+            ]
+          },
+          {
+            name: COLLECTIONS.TAGS,
+            data: [
+              {
+                _key: 'tag1',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                name: 'Tag',
+                type: ENUMS.TAG_TYPES.EXPERTISE
+              }
+            ]
+          },
+          {
+            name: COLLECTIONS.ROLE_TAGS,
+            data: [
+              {
+                _id: 'tags/123',
+                _rev: '_WpP1l3W---',
+                _key: '123',
+                created: '2018-02-01T01:02:03.456Z',
+                modified: '2018-03-02T02:03:04.567Z',
+                source: ENUMS.DATA_SOURCES.MANUAL,
+                role: 'role1',
+                tag: 'tag1',
+                batchSize: 100,
+                skip: 0
+              }
+            ]
+          }
+        ])
+      })
+
+      genericExpectationsForTable(TABLES.TAGS)
+
+      it('should transfer all scalar properties', async () => {
+        const roleTags = await sql.select().from(TABLES.ROLE_TAGS)
+        expect(roleTags[0]).to.have.property('source', ENUMS.DATA_SOURCES.MANUAL)
+      })
+
+      it('should remap the relations', async () => {
+        const roleTags = await sql.select().from(TABLES.ROLE_TAGS)
+        const roles = await sql.select().from(TABLES.ROLES)
+        const tags = await sql.select().from(TABLES.TAGS)
+        expect(roleTags[0]).to.have.property('role', roles[0].id)
+        expect(roleTags[0]).to.have.property('tag', tags[0].id)
+      })
+    })
+  })
+})

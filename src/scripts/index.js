394c20217d6566ece2bd74af12d2e9e772aa4c6b
Timothy Robinson <timmyrobby@gmail.com>
2018-05-14 14:37:20 +0100

    minor: Add noSQL db to integration test setup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,22 @@
 require('envkey')
 const { Database } = require('arangojs')
-const db = new Database({
-  url: 'http://db:8529'
-})
-const [scriptName] = process.argv.slice(2)
+const { DB_URL, NO_SQL_URL } = require('../gql/lib/constants')
 
+const db = new Database({ url: DB_URL })
 db.useDatabase(process.env.DB_NAME)
 db.useBasicAuth(process.env.DB_USER, process.env.DB_PASS)
+
+const noSQL = new Database({ url: NO_SQL_URL })
+noSQL.useDatabase(process.env.NO_SQL_NAME)
+noSQL.useBasicAuth(process.env.NO_SQL_USER, process.env.NO_SQL_PASS)
+
+const [ scriptName, arg ] = process.argv.slice(2, 4)
 
 const script = require(`./${scriptName}`);
 
 (async () => {
   try {
-    await script({ db })
+    await script({ db, noSQL, arg })
   } catch (error) {
     console.log('\n\n', error)
   }
--- src/test/integration/lib/index.js
+++ src/test/integration/lib/index.js
@@ -1,17 +1,20 @@
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
+const chaiAsPromised = require('chai-as-promised')
 
-const db = require('./db')
+const { db, noSQL } = require('./db')
 const { setupCollections, populateCollections } = require('./setup')
 const { truncateCollections, teardownCollections } = require('./teardown')
 
 const { expect } = chai
 
 chai.use(dirtyChai)
+chai.use(chaiAsPromised)
 
 module.exports = {
   expect,
   db,
+  noSQL,
   setupCollections,
   truncateCollections,
   teardownCollections,

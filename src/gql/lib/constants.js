ba52d1d41cfcbbf5e7907fd02e3f3eb9af7927a5
Timothy Robinson <timmyrobby@gmail.com>
2018-05-14 17:02:20 +0100

    minor: Add NO_SQL_URL constant

--- src/gql/lib/constants.js
+++ src/gql/lib/constants.js
@@ -1,9 +1,11 @@
 const NUDJ = 'NUDJ'
 const DB_URL = `http://${process.env.DB_HOST}:${process.env.DB_PORT}`
+const NO_SQL_URL = `http://${process.env.NO_SQL_HOST}:${process.env.NO_SQL_PORT}`
 const INTERNAL_EMAIL_ADDRESS = 'hello@nudj.co'
 
 module.exports = {
   NUDJ,
   DB_URL,
+  NO_SQL_URL,
   INTERNAL_EMAIL_ADDRESS
 }

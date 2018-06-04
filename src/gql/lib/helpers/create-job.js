36f05af369d304417cdccf7532f85ffeae6ef1bc
Timothy Robinson <timmyrobby@gmail.com>
2018-06-04 16:16:37 +0100

    minor: Extract common job creation logic to helper

---
+++ src/gql/lib/helpers/create-job.js
@@ -0,0 +1,54 @@
+const omit = require('lodash/omit')
+
+const makeUniqueSlug = require('./make-unique-slug')
+const { values: tagTypes } = require('../../schema/enums/tag-types')
+const { values: tagSources } = require('../../schema/enums/tag-sources')
+
+const createJob = async (context, company, data) => {
+  const { tags } = data
+  const slug = await makeUniqueSlug({
+    type: 'jobs',
+    data: data,
+    context
+  })
+
+  const job = await context.store.create({
+    type: 'jobs',
+    data: {
+      ...omit(data, ['tags']),
+      slug,
+      company: company.id
+    }
+  })
+
+  if (tags) {
+    const jobTags = await Promise.all(tags.map(tag => {
+      return context.store.readOneOrCreate({
+        type: 'tags',
+        filters: {
+          name: tag,
+          type: tagTypes.EXPERTISE
+        },
+        data: {
+          name: tag,
+          type: tagTypes.EXPERTISE
+        }
+      })
+    }))
+
+    await Promise.all(jobTags.map(tag => {
+      return context.store.create({
+        type: 'jobTags',
+        data: {
+          job: job.id,
+          tag: tag.id,
+          source: tagSources.NUDJ
+        }
+      })
+    }))
+  }
+
+  return job
+}
+
+module.exports = createJob
--- src/gql/schema/company/createJob.js
+++ src/gql/schema/company/createJob.js
@@ -1,9 +1,5 @@
-const omit = require('lodash/omit')
-
-const makeUniqueSlug = require('../../lib/helpers/make-unique-slug')
+const createJob = require('../../lib/helpers/create-job')
 const handleErrors = require('../../lib/handle-errors')
-const { values: tagTypes } = require('../enums/tag-types')
-const { values: tagSources } = require('../enums/tag-sources')
 
 module.exports = {
   typeDefs: `
@@ -14,50 +10,7 @@ module.exports = {
   resolvers: {
     Company: {
       createJob: handleErrors(async (company, args, context) => {
-        const { tags } = args.data
-        const slug = await makeUniqueSlug({
-          type: 'jobs',
-          data: args.data,
-          context
-        })
-
-        const job = await context.store.create({
-          type: 'jobs',
-          data: {
-            ...omit(args.data, ['tags']),
-            slug,
-            company: company.id
-          }
-        })
-
-        if (tags) {
-          const jobTags = await Promise.all(tags.map(tag => {
-            return context.store.readOneOrCreate({
-              type: 'tags',
-              filters: {
-                name: tag,
-                type: tagTypes.EXPERTISE
-              },
-              data: {
-                name: tag,
-                type: tagTypes.EXPERTISE
-              }
-            })
-          }))
-
-          await Promise.all(jobTags.map(tag => {
-            return context.store.create({
-              type: 'jobTags',
-              data: {
-                job: job.id,
-                tag: tag.id,
-                source: tagSources.NUDJ
-              }
-            })
-          }))
-        }
-
-        return job
+        return createJob(context, company, args.data)
       })
     }
   }

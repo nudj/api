1f9bd8478d6fda0b4425cbd4f49db9fdd8c89336
Nick Collings <nick@collingo.com>
2018-06-04 15:51:15 +0100

    minor: Add Company.surveyByFiltersOrDefault resolver

---
+++ src/gql/schema/company/surveyByFiltersOrDefault.js
@@ -0,0 +1,43 @@
+const { handleErrors } = require('../../lib')
+
+module.exports = {
+  typeDefs: `
+    extend type Company {
+      surveyByFiltersOrDefault (
+        filters: SurveyFilterInput!
+      ): Survey
+    }
+  `,
+  resolvers: {
+    Company: {
+      surveyByFiltersOrDefault: handleErrors(async (company, args, context) => {
+        if (args.filters.slug !== 'default') {
+          const [
+            edge,
+            filteredSurvey
+          ] = await Promise.all([
+            context.store.readOne({
+              type: 'companySurveys',
+              filters: {
+                company: company.id
+              }
+            }),
+            context.store.readOne({
+              type: 'surveys',
+              filters: args.filters
+            })
+          ])
+          if (edge && filteredSurvey && edge.survey === filteredSurvey.id) {
+            return filteredSurvey
+          }
+        }
+        return context.store.readOne({
+          type: 'surveys',
+          filters: {
+            slug: 'default'
+          }
+        })
+      })
+    }
+  }
+}

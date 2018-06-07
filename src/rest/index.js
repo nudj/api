2d30a39f885dbd56fae4e9eeb99646ad0a27683d
Timothy Robinson <timmyrobby@gmail.com>
2018-06-07 11:15:00 +0100

    patch: Update restify to v5
    patch: Add `mapParams` option to restify plugins

--- src/rest/index.js
+++ src/rest/index.js
@@ -5,8 +5,12 @@ const routes = require('./routes')
 
 const server = restify.createServer()
 server.use(morgan('combined'))
-server.use(restify.bodyParser())
-server.use(restify.queryParser())
+server.use(restify.plugins.bodyParser({
+  mapParams: true
+}))
+server.use(restify.plugins.queryParser({
+  mapParams: true
+}))
 server.use(parseQueryBooleans)
 routes(server)
 module.exports = server

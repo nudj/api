34c866d1ba10be4851f8a3e4d84728325d8dbb35
Richard Palmer <rich@rdjpalmer.com>
2018-06-11 15:51:33 +0100

    minor: Update `hirer.setOnboarded` to support onboarded: false

--- src/gql/schema/hirer/setOnboarded.js
+++ src/gql/schema/hirer/setOnboarded.js
@@ -1,17 +1,21 @@
+const isNil = require('lodash/isNil')
+
 module.exports = {
   typeDefs: `
     extend type Hirer {
-      setOnboarded: Boolean!
+      setOnboarded(onboard: Boolean): Boolean
     }
   `,
   resolvers: {
     Hirer: {
       setOnboarded: async (hirer, args, context) => {
+        const onboard = isNil(args.onboard) ? true : args.onboard
+
         const updatedHirer = await context.store.update({
           type: 'hirers',
           id: hirer.id,
           data: {
-            onboarded: true
+            onboarded: onboard
           }
         })
 

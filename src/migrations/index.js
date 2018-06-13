a6704ce44d75c3e91141332e30d38c9e33028b21
Timothy Robinson <timmyrobby@gmail.com>
2018-06-13 09:41:16 +0100

    minor: Pass noSQL db to migration scripts

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,12 +1,17 @@
 require('envkey')
 const { Database } = require('arangojs')
-const db = new Database({
-  url: 'http://db:8529'
-})
+
+const { DB_URL, NO_SQL_URL } = require('../gql/lib/constants')
+
+const db = new Database({ url: DB_URL })
 const [migrationName, direction] = process.argv.slice(2)
 
 db.useDatabase(process.env.DB_NAME)
 db.useBasicAuth(process.env.DB_USER, process.env.DB_PASS)
+
+const noSQL = new Database({ url: NO_SQL_URL })
+noSQL.useDatabase(process.env.NO_SQL_NAME)
+noSQL.useBasicAuth(process.env.NO_SQL_USER, process.env.NO_SQL_PASS)
 
 const migration = require(`./${migrationName}`)
 
@@ -18,7 +23,7 @@ async function step (description, actions) {
 
 (async () => {
   try {
-    await migration[direction]({ db, step })
+    await migration[direction]({ db, noSQL, step })
   } catch (error) {
     console.log('\n\n', error)
   }

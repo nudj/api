8d7042ad47ab22b3f780bc1d399da424563bdafa
Timothy Robinson <timmyrobby@gmail.com>
2018-06-27 16:36:02 +0100

    patch: Add employment handling to addCompanyAndAssignUserAsHirer

--- src/gql/schema/person/addCompanyAndAssignUserAsHirer.js
+++ src/gql/schema/person/addCompanyAndAssignUserAsHirer.js
@@ -1,6 +1,7 @@
 const makeUniqueSlug = require('../../lib/helpers/make-unique-slug')
 const { handleErrors } = require('../../lib')
 const { values: hirerTypes } = require('../enums/hirer-types')
+const { values: dataSources } = require('../enums/data-sources')
 
 module.exports = {
   typeDefs: `
@@ -64,6 +65,38 @@ module.exports = {
               description,
               client,
               onboarded: false
+            }
+          })
+        }
+
+        const currentEmployment = await context.store.readOne({
+          type: 'employments',
+          filters: {
+            person: person.id,
+            current: true
+          }
+        })
+
+        const employedWithOtherCompany = currentEmployment && currentEmployment.company !== company.id
+
+        if (employedWithOtherCompany) {
+          await context.store.update({
+            type: 'employments',
+            id: currentEmployment.id,
+            data: {
+              current: false
+            }
+          })
+        }
+
+        if (!currentEmployment || employedWithOtherCompany) {
+          await context.store.create({
+            type: 'employments',
+            data: {
+              person: person.id,
+              company: company.id,
+              current: true,
+              source: dataSources.NUDJ
             }
           })
         }

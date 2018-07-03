0a7da90952e870124932477d7ca4cea6a38114db
Timothy Robinson <timmyrobby@gmail.com>
2018-07-03 11:10:54 +0100

    minor: Extract invited member setup to helper function

--- src/gql/schema/company/inviteMembers.js
+++ src/gql/schema/company/inviteMembers.js
@@ -1,15 +1,16 @@
-const uniq = require('lodash/uniq')
 const chunk = require('lodash/chunk')
 const promiseSerial = require('promise-serial')
 const hashGenerator = require('hash-generator')
 
 const { logger } = require('@nudj/library')
 
-const fetchPerson = require('../../lib/helpers/fetch-person')
 const { handleErrors } = require('../../lib')
 const {
+  validateInviteesAndFetchEmailData,
+  logInvitationsToIntercom
+} = require('../../lib/helpers')
+const {
   send,
-  sendInternalEmail,
   teammateInvitationEmailBodyTemplate
 } = require('../../lib/mailer')
 const intercom = require('../../lib/intercom')
@@ -69,65 +70,15 @@ module.exports = {
   resolvers: {
     Company: {
       inviteMembers: handleErrors(async (company, args, context) => {
-        const emailAddresses = uniq(args.emailAddresses)
-
-        if (!emailAddresses.length) {
-          throw new Error('No email addresses provided')
-        }
-
-        if (!company.hash) {
-          company = await context.store.update({
-            type: 'companies',
-            id: company.id,
-            data: {
-              hash: hashGenerator(128)
-            }
-          })
-        }
-
-        // ensure the people being invited are not already hirers at another company
-        await Promise.all(emailAddresses.map(async email => {
-          const person = await context.store.readOne({
-            type: 'people',
-            filters: { email }
-          })
-
-          if (!person) return
-
-          const hirer = await context.store.readOne({
-            type: 'hirers',
-            filters: { person: person.id }
-          })
-
-          if (!hirer) return
-
-          if (hirer.company !== company.id) {
-            await sendInternalEmail({
-              subject: 'Teammate invitation failed',
-              html: `A hirer for ${company.name} attempted to add a teammate with the email address "${email}", but that email address is already related to a different company with id: "${hirer.company}"`
-            })
-            throw new Error(`User with email address "${email}" is already signed up with another company`)
-          }
-        }))
-
+        const emailData = await validateInviteesAndFetchEmailData(company, args, context)
         const {
-          firstName,
-          lastName,
-          email: senderEmail
-        } = await fetchPerson(context, context.userId)
-        const senderName = firstName && lastName && `${firstName} ${lastName}`
-        const from = senderName
-          ? `${senderName} via nudj <hello@nudj.co>`
-          : `nudj <hello@nudj.co>`
-        const subject = senderName
-          ? `Help ${senderName} and the rest of ${company.name} hire more great people using nudj`
-          : `You've been invited to join nudj!`
-        const jobs = await context.store.readAll({
-          type: 'jobs',
-          filters: {
-            company: company.id
-          }
-        })
+          from,
+          subject,
+          senderName,
+          senderEmail,
+          jobs,
+          emailAddresses
+        } = emailData
 
         const [ sendStatus ] = await Promise.all(
           emailAddresses.map(email => send({
@@ -146,19 +97,9 @@ module.exports = {
 
         try {
           if (args.awaitIntercom) {
-            await triggerIntercomTracking({
-              company,
-              emailAddresses,
-              senderName,
-              senderEmail
-            })
+            await triggerIntercomTracking(emailData)
           } else {
-            triggerIntercomTracking({
-              company,
-              emailAddresses,
-              senderName,
-              senderEmail
-            })
+            triggerIntercomTracking(emailData)
           }
         } catch (error) {
           logger('error', 'Intercom error', error)

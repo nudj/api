405f6f7945cdb1235594697ad1d661d5c2781f9f
Nick Collings <nick@collingo.com>
2018-07-06 10:51:56 +0100

    patch: Restore support for arg in execute commands

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,22 +7,21 @@ const db = new Database({ url: OLD_DB_URL })
 db.useDatabase(process.env.DB_NAME)
 db.useBasicAuth(process.env.DB_USER, process.env.DB_PASS)
 
-const [ scriptName, target ] = process.argv.slice(2, 4)
+const [ scriptName, target, arg ] = process.argv.slice(2, 5)
 
-const host = target ? process.env[`${target.toUpperCase()}_SQL_HOST`] : process.env.SQL_HOST
-const port = target ? process.env[`${target.toUpperCase()}_SQL_PORT`] : process.env.SQL_PORT
-const user = target ? process.env[`${target.toUpperCase()}_SQL_USER`] : process.env.SQL_USER
-const password = target ? process.env[`${target.toUpperCase()}_SQL_PASS`] : process.env.SQL_PASS
-const database = target ? process.env[`${target.toUpperCase()}_SQL_NAME`] : process.env.SQL_NAME
+let prefix = ''
+if (['test', 'staging', 'development'].includes(target)) {
+  prefix = `${target.toUpperCase()}_`
+}
 
 const sql = knex({
   client: 'mysql',
   connection: {
-    host,
-    port,
-    user,
-    password,
-    database,
+    host: process.env[`${prefix}SQL_HOST`],
+    port: process.env[`${prefix}SQL_PORT`],
+    user: process.env[`${prefix}SQL_USER`],
+    password: process.env[`${prefix}SQL_PASS`],
+    database: process.env[`${prefix}SQL_NAME`],
     charset: 'utf8mb4'
   }
 })
@@ -36,7 +35,7 @@ const script = require(`./${scriptName}`);
 (async () => {
   let exitCode = 0
   try {
-    await script({ db, sql, nosql })
+    await script({ db, sql, nosql, arg })
   } catch (error) {
     console.log('\n\n', error)
     exitCode = 1

e3e41daf85ec5e2e6fa5b8279d8e31d58fd5ad41
Nick Collings <nick@collingo.com>
2018-07-16 16:35:05 +0100

    major: Update Company.inviteMembers to support group login flow

--- src/test/unit/schema/company/inviteMembers.js
+++ src/test/unit/schema/company/inviteMembers.js
@@ -5,7 +5,6 @@ const sinon = require('sinon')
 const expect = chai.expect
 
 const schema = require('../../../../gql/schema')
-const { values: hirerTypes } = require('../../../../gql/schema/enums/hirer-types')
 const { executeQueryOnDbUsingSchema, shouldRespondWithGqlError } = require('../../helpers')
 
 describe('Company.inviteMembers', () => {
@@ -142,52 +141,22 @@ describe('Company.inviteMembers', () => {
     })
   })
 
-  describe('when the person exists', () => {
-    it('should not create a person', async () => {
+  describe('when the person is already a hirer of another company', () => {
+    it('should throw an error and notify nudj if the hirer is with a different company', async () => {
       const db = {
         companies: [
           {
             id: 'company1',
-            name: 'Fake Company'
+            name: 'Umbrella Corporation'
           }
         ],
-        hirers: [],
-        people: [
+        hirers: [
           {
-            id: 'person1',
-            email: 'tim@nudj.co'
-          },
-          {
-            id: 'person2',
-            email: 'jim@nudj.co'
-          },
-          {
-            id: 'person3',
-            email: 'jamie@nudj.co'
-          }
-        ]
-      }
-
-      expect(db.people.length).to.equal(3)
-
-      await executeQueryOnDbUsingSchema({ operation, db, variables, schema })
-
-      expect(db.people.length).to.deep.equal(3)
-      expect(db.people[0]).to.deep.equal({
-        id: 'person1',
-        email: 'tim@nudj.co'
-      })
-    })
-
-    it('should create a hirer', async () => {
-      const db = {
-        companies: [
-          {
-            id: 'company1',
-            name: 'Fake Company'
+            id: 'hirer1',
+            person: 'person2',
+            company: 'company3'
           }
         ],
-        hirers: [],
         people: [
           {
             id: 'person1',
@@ -196,174 +165,22 @@ describe('Company.inviteMembers', () => {
           },
           {
             id: 'person2',
+            firstName: 'Tim',
+            lastName: 'Robinson',
             email: 'tim@nudj.co'
           }
         ]
       }
 
-      await executeQueryOnDbUsingSchema({ operation, db, variables, schema })
+      const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      const notifyEmailBody = decodeURI(mailerStub.getCall(0).args)
 
-      expect(db.hirers[0]).to.deep.equal({
-        id: 'hirer1',
-        company: 'company1',
-        onboarded: false,
-        person: 'person2',
-        type: hirerTypes.MEMBER
-      })
-    })
-
-    describe('when the person is already a hirer', () => {
-      it('should not create a hirer', async () => {
-        const db = {
-          companies: [
-            {
-              id: 'company1',
-              name: 'Fake Company'
-            }
-          ],
-          hirers: [
-            {
-              id: 'hirer1',
-              person: 'person1',
-              company: 'company1'
-            },
-            {
-              id: 'hirer2',
-              person: 'person2',
-              company: 'company2'
-            },
-            {
-              id: 'hirer3',
-              person: 'person3',
-              company: 'company3'
-            }
-          ],
-          people: [
-            {
-              id: 'person1',
-              firstName: 'David',
-              lastName: 'Platt'
-            },
-            {
-              id: 'person1',
-              email: 'tim@nudj.co'
-            },
-            {
-              id: 'person2',
-              email: 'jim@nudj.co'
-            },
-            {
-              id: 'person3',
-              email: 'jamie@nudj.co'
-            }
-          ]
-        }
-
-        expect(db.hirers.length).to.equal(3)
-
-        await executeQueryOnDbUsingSchema({ operation, db, variables, schema })
-
-        expect(db.hirers.length).to.equal(3)
-        expect(db.hirers[0]).to.deep.equal({
-          id: 'hirer1',
-          person: 'person1',
-          company: 'company1'
-        })
-      })
-
-      it('should throw an error and notify nudj if the hirer is with a different company', async () => {
-        const db = {
-          companies: [
-            {
-              id: 'company1',
-              name: 'Umbrella Corporation'
-            }
-          ],
-          hirers: [
-            {
-              id: 'hirer1',
-              person: 'person2',
-              company: 'company3'
-            }
-          ],
-          people: [
-            {
-              id: 'person1',
-              firstName: 'David',
-              lastName: 'Platt'
-            }
-          ]
-        }
-
-        const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
-        const notifyEmailBody = decodeURI(mailerStub.getCall(0).args)
-
-        expect(notifyEmailBody).to.include('subject=Teammate invitation failed')
-        expect(notifyEmailBody).to.include('html=A hirer for Umbrella Corporation attempted to add a teammate')
-        expect(mailerStub.callCount).to.equal(1)
-        shouldRespondWithGqlError({
-          path: ['company', 'inviteMembers']
-        })(result)
-      })
-    })
-  })
-
-  describe('when the person does not exist', () => {
-    it('should create a person', async () => {
-      const db = {
-        companies: [
-          {
-            id: 'company1',
-            name: 'Fake Company'
-          }
-        ],
-        hirers: [],
-        people: [
-          {
-            id: 'person1',
-            firstName: 'David',
-            lastName: 'Platt'
-          }
-        ]
-      }
-
-      expect(db.people.length).to.equal(1)
-
-      await executeQueryOnDbUsingSchema({ operation, db, variables, schema })
-
-      expect(db.people[1]).to.deep.equal({
-        id: 'person2',
-        email: 'tim@nudj.co'
-      })
-    })
-
-    it('should create a hirer', async () => {
-      const db = {
-        companies: [
-          {
-            id: 'company1',
-            name: 'Fake Company'
-          }
-        ],
-        hirers: [],
-        people: [
-          {
-            id: 'person1',
-            firstName: 'David',
-            lastName: 'Platt'
-          }
-        ]
-      }
-
-      await executeQueryOnDbUsingSchema({ operation, db, variables, schema })
-
-      expect(db.hirers[0]).to.deep.equal({
-        id: 'hirer1',
-        company: 'company1',
-        onboarded: false,
-        person: 'person2',
-        type: hirerTypes.MEMBER
-      })
+      expect(notifyEmailBody).to.include('subject=Teammate invitation failed')
+      expect(notifyEmailBody).to.include('html=A hirer for Umbrella Corporation attempted to add a teammate')
+      expect(mailerStub.callCount).to.equal(1)
+      shouldRespondWithGqlError({
+        path: ['company', 'inviteMembers']
+      })(result)
     })
   })
 

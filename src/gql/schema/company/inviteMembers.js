3e86ad2424dbc3b6f59fcf1a3e1418e225e1c890
Timothy Robinson <timmyrobby@gmail.com>
2018-07-17 17:12:18 +0100

    major: Update `inviteMembers` with new input type and helpers

--- src/gql/schema/company/inviteMembers.js
+++ src/gql/schema/company/inviteMembers.js
@@ -1,123 +1,38 @@
-const uniq = require('lodash/uniq')
-const { logger } = require('@nudj/library')
-const hashGenerator = require('hash-generator')
-
-const fetchPerson = require('../../lib/helpers/fetch-person')
 const { handleErrors } = require('../../lib')
 const {
+  validateInviteesAndFetchEmailData,
+  logInvitationsToIntercom
+} = require('../../lib/helpers')
+const {
   send,
-  sendInternalEmail,
   teammateInvitationEmailBodyTemplate
 } = require('../../lib/mailer')
-const intercom = require('../../lib/intercom')
-
-const triggerIntercomTracking = async ({
-  company,
-  emailAddresses,
-  senderName,
-  senderEmail
-}) => {
-  const intercomCompany = await intercom.fetchOrCreateCompanyByName(company.name)
-
-  await emailAddresses.map(async email => {
-    await intercom.createUniqueUserAndTag({
-      email,
-      companies: [
-        {
-          name: intercomCompany.name,
-          company_id: intercomCompany.company_id
-        }
-      ]
-    }, 'team-member')
-    await intercom.logEvent({
-      event_name: 'invited',
-      email,
-      metadata: {
-        category: 'onboarding',
-        invited_by: senderName || senderEmail
-      }
-    })
-  })
-  await intercom.logEvent({
-    event_name: 'invites-sent',
-    email: senderEmail,
-    metadata: {
-      category: 'onboarding'
-    }
-  })
-}
 
 module.exports = {
   typeDefs: `
+    input InviteMemberPersonInput {
+      email: String!
+      firstName: String
+    }
+
     extend type Company {
-      inviteMembers(emailAddresses: [String!]!): Status
+      inviteMembers(members: [InviteMemberPersonInput!]!): Status
     }
   `,
   resolvers: {
     Company: {
       inviteMembers: handleErrors(async (company, args, context) => {
-        const emailAddresses = uniq(args.emailAddresses)
-
-        if (!emailAddresses.length) {
-          throw new Error('No email addresses provided')
-        }
-
-        if (!company.hash) {
-          company = await context.sql.update({
-            type: 'companies',
-            id: company.id,
-            data: {
-              hash: hashGenerator(128)
-            }
-          })
-        }
-
-        // ensure the people being invited are not already hirers at another company
-        await Promise.all(emailAddresses.map(async email => {
-          const person = await context.sql.readOne({
-            type: 'people',
-            filters: { email }
-          })
-
-          if (!person) return
-
-          const hirer = await context.sql.readOne({
-            type: 'hirers',
-            filters: { person: person.id }
-          })
-
-          if (!hirer) return
-
-          if (hirer.company !== company.id) {
-            await sendInternalEmail({
-              subject: 'Teammate invitation failed',
-              html: `A hirer for ${company.name} attempted to add a teammate with the email address "${email}", but that email address is already related to a different company with id: "${hirer.company}"`
-            })
-            throw new Error(`User with email address "${email}" is already signed up with another company`)
-          }
-        }))
-
+        const emailData = await validateInviteesAndFetchEmailData(company, args, context)
         const {
-          firstName,
-          lastName,
-          email: senderEmail
-        } = await fetchPerson(context, context.userId)
-        const senderName = firstName && lastName && `${firstName} ${lastName}`
-        const from = senderName
-          ? `${senderName} via nudj <hello@nudj.co>`
-          : `Nudj <hello@nudj.co>`
-        const subject = senderName
-          ? `${senderName} has invited you to join them on nudj`
-          : `You've been invited to join nudj!`
-        const jobs = await context.sql.readAll({
-          type: 'jobs',
-          filters: {
-            company: company.id
-          }
-        })
+          from,
+          subject,
+          senderName,
+          jobs,
+          members
+        } = emailData
 
         const [ sendStatus ] = await Promise.all(
-          emailAddresses.map(email => send({
+          members.map(({ email }) => send({
             from,
             to: email,
             subject,
@@ -131,15 +46,10 @@ module.exports = {
           }))
         )
 
-        try {
-          triggerIntercomTracking({
-            company,
-            emailAddresses,
-            senderName,
-            senderEmail
-          })
-        } catch (error) {
-          logger('error', 'Intercom error', error)
+        if (args.awaitIntercom) {
+          await logInvitationsToIntercom(emailData)
+        } else {
+          logInvitationsToIntercom(emailData)
         }
 
         return sendStatus

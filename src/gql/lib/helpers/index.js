b069daad60244179632bb7693cc162e752313472
Timothy Robinson <timmyrobby@gmail.com>
2018-07-17 17:10:35 +0100

    minor: Add helpers/index.js

---
+++ src/gql/lib/helpers/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  connectionsIndexFormatter: require('./connections-index-formatter'),
+  createJob: require('./create-job'),
+  createPerson: require('./create-person'),
+  fetchConnectionPropertyMap: require('./fetch-connection-property-map'),
+  fetchPerson: require('./fetch-person'),
+  fetchReferral: require('./fetch-referral'),
+  fetchRoleTagMaps: require('./fetch-role-tag-maps'),
+  formatLinkedinConnections: require('./format-linkedin-connections'),
+  makeSlug: require('./make-slug'),
+  makeUniqueCompanySlug: require('./make-unique-company-slug'),
+  makeUniqueSlug: require('./make-unique-slug'),
+  notifyTeamAboutJob: require('./notify-team-about-job'),
+  updatePerson: require('./update-person'),
+  logInvitationsToIntercom: require('./log-invitations-to-intercom'),
+  validateInviteesAndFetchEmailData: require('./validate-invitees-and-fetch-email-data')
+}

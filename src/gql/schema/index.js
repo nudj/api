7deacc4df866ada1cf0af8c18e1e72ef62e12245
Timothy Robinson <timmyrobby@gmail.com>
2018-07-31 22:44:46 +0100

    minor: Add `accessRequestByFilters` resolvers

--- src/gql/schema/index.js
+++ src/gql/schema/index.js
@@ -13,6 +13,7 @@ const modules = [
   require('./application/person'),
   require('./application/referral'),
   require('./company'),
+  require('./company/accessRequestByFilters'),
   require('./company/createHirerByEmail'),
   require('./company/createAccessRequest'),
   require('./company/createJob'),
@@ -144,6 +145,7 @@ const modules = [
   // Mutations
   require('./mutation'),
   require('./mutation/accessRequest'),
+  require('./mutation/accessRequestByFilters'),
   require('./mutation/application'),
   require('./mutation/applicationByFilters'),
   require('./mutation/applications'),
@@ -226,6 +228,7 @@ const modules = [
   // Queries
   require('./query'),
   require('./query/accessRequest'),
+  require('./query/accessRequestByFilters'),
   require('./query/application'),
   require('./query/applicationByFilters'),
   require('./query/applications'),

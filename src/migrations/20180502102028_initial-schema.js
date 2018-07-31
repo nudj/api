1f85f5f5533b615df826f6ea470acff280ae9d4c
Nick Collings <nick@collingo.com>
2018-07-31 15:23:05 +0100

    patch: Fix name of acceptedAccessRequests table

--- src/migrations/20180502102028_initial-schema.js
+++ src/migrations/20180502102028_initial-schema.js
@@ -440,16 +440,16 @@ exports.up = async knex => {
       t.unique([COMPANY, PERSON], INDICES[TABLES.ACCESS_REQUESTS][[COMPANY, PERSON].join('')].name)
     })
 
-    .createTable(TABLES.ACCEPT_ACCESS_REQUESTS, t => {
+    .createTable(TABLES.ACCEPTED_ACCESS_REQUESTS, t => {
       const {
         ACCESS_REQUEST,
         HIRER
-      } = FIELDS[TABLES.ACCEPT_ACCESS_REQUESTS]
+      } = FIELDS[TABLES.ACCEPTED_ACCESS_REQUESTS]
 
       defaultConfig(t, knex)
       relationType(ACCESS_REQUEST, TABLES.COMPANIES, t, knex).notNullable()
       relationType(HIRER, TABLES.PEOPLE, t, knex).notNullable()
-      t.unique([ACCESS_REQUEST, HIRER], INDICES[TABLES.ACCEPT_ACCESS_REQUESTS][[ACCESS_REQUEST, HIRER].join('')].name)
+      t.unique([ACCESS_REQUEST, HIRER], INDICES[TABLES.ACCEPTED_ACCESS_REQUESTS][[ACCESS_REQUEST, HIRER].join('')].name)
     })
 
   // create all required collections in the nosql store
@@ -467,7 +467,7 @@ exports.up = async knex => {
 
 exports.down = async knex => {
   await knex.schema
-    .dropTable(TABLES.ACCEPT_ACCESS_REQUESTS)
+    .dropTable(TABLES.ACCEPTED_ACCESS_REQUESTS)
     .dropTable(TABLES.ACCESS_REQUESTS)
     .dropTable(TABLES.RELATED_JOBS)
     .dropTable(TABLES.SURVEY_QUESTION_TAGS)

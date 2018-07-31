1204b6ef79fbbd41154232948ffe2adfb00870e2
Timothy Robinson <timmyrobby@gmail.com>
2018-07-31 11:35:16 +0100

    minor: Add `createAccessRequest` resolver

---
+++ src/gql/schema/company/createAccessRequest.js
@@ -0,0 +1,50 @@
+const { values: hirerTypes } = require('../enums/hirer-types')
+const mailer = require('../../lib/mailer')
+const fetchPerson = require('../../lib/helpers/fetch-person')
+
+module.exports = {
+  typeDefs: `
+    extend type Company {
+      createAccessRequest(person: ID!): AccessRequest
+    }
+  `,
+  resolvers: {
+    Company: {
+      createAccessRequest: async (company, args, context) => {
+        const accessRequest = await context.sql.create({
+          type: 'accessRequests',
+          data: {
+            company: company.id,
+            person: args.person
+          }
+        })
+
+        const adminHirers = await context.sql.readAll({
+          type: 'hirers',
+          filters: {
+            company: company.id,
+            type: hirerTypes.ADMIN
+          }
+        })
+        const adminPeople = await context.sql.readMany({
+          type: 'people',
+          ids: adminHirers.map(admin => admin.person)
+        })
+
+        const requestee = await fetchPerson(context, args.person)
+
+        await Promise.all(
+          adminPeople.map(admin => mailer.sendAccessRequestEmail({
+            to: admin.email,
+            hire: context.hire,
+            requestee,
+            requestId: accessRequest.id,
+            company
+          }))
+        )
+
+        return accessRequest
+      }
+    }
+  }
+}

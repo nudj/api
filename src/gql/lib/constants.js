5fa50182bcf51078e1b58c5668ea0286a83b8779
Timothy Robinson <timmyrobby@gmail.com>
2018-08-07 13:41:28 +0100

    minor: Add `DUMMY_APPLICANT_EMAIL_ADDRESS` constant

--- src/gql/lib/constants.js
+++ src/gql/lib/constants.js
@@ -5,6 +5,7 @@ const NUDJ = 'NUDJ'
 const DB_URL = `http://${process.env.DB_HOST}:${process.env.DB_PORT}`
 const NO_SQL_URL = `http://${process.env.NO_SQL_HOST}:${process.env.NO_SQL_PORT}`
 const INTERNAL_EMAIL_ADDRESS = 'hello@nudj.co'
+const DUMMY_APPLICANT_EMAIL_ADDRESS = 'hello+applicant@nudj.co'
 const INTERNAL_EMAIL_FROM = `nudj <${INTERNAL_EMAIL_ADDRESS}>`
 
 const randomSlugGenerator = () => hash(10)
@@ -29,5 +30,6 @@ module.exports = {
   NO_SQL_URL,
   INTERNAL_EMAIL_ADDRESS,
   INTERNAL_EMAIL_FROM,
-  SLUG_GENERATORS
+  SLUG_GENERATORS,
+  DUMMY_APPLICANT_EMAIL_ADDRESS
 }

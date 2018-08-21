c063cd21f4a6abb14c6d9a27e9c31f9d34a9f55f
Nick Collings <nick@collingo.com>
2018-08-21 18:42:20 +0100

    minor: Move full job slug generation to publish time only, add draft slug for draft jobs

--- src/gql/lib/helpers/create-job.js
+++ src/gql/lib/helpers/create-job.js
@@ -1,16 +1,24 @@
 const omit = require('lodash/omit')
 
 const makeUniqueSlug = require('./make-unique-slug')
+const { random: randomSlugGenerator } = require('./slug-generators')
+const { values: jobStatusTypes } = require('../../schema/enums/job-status-types')
 const { values: tagTypes } = require('../../schema/enums/tag-types')
 const { values: tagSources } = require('../../schema/enums/tag-sources')
 
 const createJob = async (context, company, data) => {
   const { tags } = data
-  const slug = await makeUniqueSlug({
-    type: 'jobs',
-    data: data,
-    context
-  })
+  let slug
+
+  if (data.status === jobStatusTypes.DRAFT) {
+    slug = `draft-${randomSlugGenerator()}`
+  } else {
+    slug = await makeUniqueSlug({
+      type: 'jobs',
+      data,
+      context
+    })
+  }
 
   const job = await context.store.create({
     type: 'jobs',

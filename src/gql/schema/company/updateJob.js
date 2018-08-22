6c1fb529f1b73e0e6c9269e4fb01f5b92056e1c2
Timothy Robinson <timmyrobby@gmail.com>
2018-08-22 10:33:02 +0100

    minor: Add intercom tracking when a company publishes jobs

--- src/gql/schema/company/updateJob.js
+++ src/gql/schema/company/updateJob.js
@@ -1,3 +1,4 @@
+const { intercom } = require('@nudj/library/analytics')
 const { values: tagTypes } = require('../enums/tag-types')
 const { values: tagSources } = require('../enums/tag-sources')
 const { values: jobStatusTypes } = require('../enums/job-status-types')
@@ -127,11 +128,23 @@ module.exports = {
           }))
         }
 
-        if (
-          notifyTeam &&
+        const isNewlyPublished = (
           existingJob.status !== jobStatusTypes.PUBLISHED &&
           updatedJob.status === jobStatusTypes.PUBLISHED
-        ) {
+        )
+
+        if (isNewlyPublished) {
+          await intercom.companies.update({
+            company: { name: company.name },
+            data: {
+              custom_attributes: {
+                'has had published job': true
+              }
+            }
+          })
+        }
+
+        if (notifyTeam && isNewlyPublished) {
           await notifyTeamAboutJob(context, company, updatedJob)
         }
 

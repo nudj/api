6ad4e56d44a22df799beed66e6a51b89970f9411
Nick Collings <nick@collingo.com>
2018-08-22 14:00:36 +0100

    patch: Fix job status order so draft renders first

--- src/gql/schema/enums/job-status-types.js
+++ src/gql/schema/enums/job-status-types.js
@@ -3,8 +3,8 @@ const { rootEnum } = require('../../lib')
 module.exports = rootEnum({
   name: 'JobStatus',
   values: [
+    'DRAFT',
     'PUBLISHED',
-    'ARCHIVED',
-    'DRAFT'
+    'ARCHIVED'
   ]
 })

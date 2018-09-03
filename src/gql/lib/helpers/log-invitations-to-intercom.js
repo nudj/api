d14f1265fd7367fcf704d95d59bafd6cd6bd8c95
Timothy Robinson <timmyrobby@gmail.com>
2018-09-03 09:41:08 +0100

    minor: Set `inviteMembers` to create intercom lead instead of user

--- src/gql/lib/helpers/log-invitations-to-intercom.js
+++ src/gql/lib/helpers/log-invitations-to-intercom.js
@@ -1,7 +1,7 @@
 const promiseSerial = require('promise-serial')
 const chunk = require('lodash/chunk')
 const { logger } = require('@nudj/library')
-const intercom = require('../intercom')
+const { intercom } = require('@nudj/library/analytics')
 
 const INTERCOM_BATCH_AMOUNT = 10
 
@@ -14,26 +14,29 @@ const logInvitationsToIntercom = async ({
   senderEmail
 }) => {
   try {
-    const intercomCompany = await intercom.fetchOrCreateCompanyByName(company.name)
+    const intercomCompany = await intercom.companies.getOrCreate({ name: company.name })
     const batchedEmails = chunk(emailAddresses, INTERCOM_BATCH_AMOUNT)
 
     await promiseSerial(batchedEmails.map(emails => async () => {
       await Promise.all(emails.map(async email => {
-        await intercom.createUniqueUserAndTag({
+        const lead = await intercom.leads.create({
           email,
           companies: [
             {
               name: intercomCompany.name,
               company_id: intercomCompany.company_id
             }
-          ]
-        }, 'team-member')
-        await intercom.logEvent({
-          event_name: 'invited',
-          email,
-          metadata: {
-            category: 'onboarding',
-            invited_by: senderName || senderEmail
+          ],
+          tags: ['team-member']
+        })
+        await intercom.leads.logEvent({
+          lead,
+          event: {
+            name: 'invited',
+            metadata: {
+              category: 'onboarding',
+              invited_by: senderName || senderEmail
+            }
           }
         })
       }))
@@ -41,11 +44,13 @@ const logInvitationsToIntercom = async ({
       return emails.length === INTERCOM_BATCH_AMOUNT && stall(10000)
     }))
 
-    return intercom.logEvent({
-      event_name: 'invites-sent',
-      email: senderEmail,
-      metadata: {
-        category: 'onboarding'
+    return intercom.users.logEvent({
+      user: { email: senderEmail },
+      event: {
+        name: 'invites-sent',
+        metadata: {
+          category: 'onboarding'
+        }
       }
     })
   } catch (error) {

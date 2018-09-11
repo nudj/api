14eaf5ff4b3903694eb3196f99f4964e39f810f2
Timothy Robinson <timmyrobby@gmail.com>
2018-09-11 09:27:19 +0100

    minor: Add `signedUp` boolean to person schema

--- src/test/unit/schema/person/filters.js
+++ src/test/unit/schema/person/filters.js
@@ -8,11 +8,13 @@ const TYPE = 'Person'
 const TYPE_PLURAL = 'people'
 const DUMMY_ID = '123'
 const DUMMY_STRING = 'abc'
+const DUMMY_BOOLEAN = true
 expectTypeIsFilterableBy = expectTypeIsFilterableBy(schema, TYPE, TYPE_PLURAL)
 
 describe('Person filters', () => {
   it('should be filterable by the following properties', async () => {
     await expectTypeIsFilterableBy('id', DUMMY_ID, 'ID')
     await expectTypeIsFilterableBy('email', DUMMY_STRING, 'String')
+    await expectTypeIsFilterableBy('signedUp', DUMMY_BOOLEAN, 'Boolean')
   })
 })

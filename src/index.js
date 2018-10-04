f23b707892b96199633068b87541b8559c84ffe5
Nick Collings <nick@collingo.com>
2018-10-04 10:00:43 +0100

    patch: Remove rest api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 require('envkey')
-const rest = require('./rest')
 const gql = require('./gql')
 const { store: sqlStore } = require('./gql/adaptors/mysql')
 
-rest.listen(81, () => console.log('Rest API running on http://localhost:81/'))
-gql({ sqlStore }).listen(82, () => console.log('GQL API running on http://localhost:82/'))
+gql({ sqlStore }).listen(80, () => console.log('GQL API running on http://localhost:80/'))

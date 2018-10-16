fbc9e8ebeb81b2b2e09338bdded5cc97c2baa679
Timothy Robinson <timmyrobby@gmail.com>
2018-10-16 15:06:51 +0100

    minor: Add validation checks to hirer deletion

--- src/gql/schema/company/deleteHirer.js
+++ src/gql/schema/company/deleteHirer.js
@@ -1,3 +1,5 @@
+const { values: hirerTypes } = require('../../schema/enums/hirer-types')
+
 module.exports = {
   typeDefs: `
     extend type Company {
@@ -6,10 +8,36 @@ module.exports = {
   `,
   resolvers: {
     Company: {
-      deleteHirer: async (company, args, context) => context.store.delete({
-        type: 'hirers',
-        id: args.id
-      })
+      deleteHirer: async (company, args, context) => {
+        const [ userHirer, hirer ] = await Promise.all([
+          context.store.readOne({
+            type: 'hirers',
+            filters: {
+              company: company.id,
+              person: context.userId
+            }
+          }),
+          context.store.readOne({
+            type: 'hirers',
+            id: args.id
+          })
+        ])
+
+        if (hirer.company !== company.id) {
+          throw new Error('Cannot delete hirers from other companies')
+        }
+        if (!userHirer || userHirer.type !== hirerTypes.ADMIN) {
+          throw new Error('User does not have permission to delete teammates')
+        }
+        if (userHirer.id === args.id) {
+          throw new Error('Cannot delete self')
+        }
+
+        return context.store.delete({
+          type: 'hirers',
+          id: args.id
+        })
+      }
     }
   }
 }

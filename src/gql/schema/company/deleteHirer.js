1abe1b9e429f70014e4a0feb47a5b46e87d2d034
Timothy Robinson <timmyrobby@gmail.com>
2018-10-16 15:21:35 +0100

    minor: Add `Company.deleteHirer` resolver

---
+++ src/gql/schema/company/deleteHirer.js
@@ -0,0 +1,43 @@
+const { values: hirerTypes } = require('../../schema/enums/hirer-types')
+
+module.exports = {
+  typeDefs: `
+    extend type Company {
+      deleteHirer(id: ID!): Hirer
+    }
+  `,
+  resolvers: {
+    Company: {
+      deleteHirer: async (company, args, context) => {
+        const [ userHirer, hirer ] = await Promise.all([
+          context.sql.readOne({
+            type: 'hirers',
+            filters: {
+              company: company.id,
+              person: context.userId
+            }
+          }),
+          context.sql.readOne({
+            type: 'hirers',
+            id: args.id
+          })
+        ])
+
+        if (hirer.company !== company.id) {
+          throw new Error('Cannot delete hirers from other companies')
+        }
+        if (!userHirer || userHirer.type !== hirerTypes.ADMIN) {
+          throw new Error('User does not have permission to delete teammates')
+        }
+        if (userHirer.id === args.id) {
+          throw new Error('Cannot delete self')
+        }
+
+        return context.sql.delete({
+          type: 'hirers',
+          id: args.id
+        })
+      }
+    }
+  }
+}

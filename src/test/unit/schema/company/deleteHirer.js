90b9df9eed2f8db22ce7db48e13bfeb92a2f0278
Timothy Robinson <timmyrobby@gmail.com>
2018-10-16 15:06:51 +0100

    minor: Add validation checks to hirer deletion

--- src/test/unit/schema/company/deleteHirer.js
+++ src/test/unit/schema/company/deleteHirer.js
@@ -3,7 +3,8 @@ const chai = require('chai')
 const expect = chai.expect
 
 const schema = require('../../../../gql/schema')
-const { executeQueryOnDbUsingSchema } = require('../../helpers')
+const { values: hirerTypes } = require('../../../../gql/schema/enums/hirer-types')
+const { executeQueryOnDbUsingSchema, shouldRespondWithGqlError } = require('../../helpers')
 
 const operation = `
   mutation DeleteHirer ($hirerId: ID!, $companyId: ID!) {
@@ -14,7 +15,7 @@ const operation = `
     }
   }
 `
-const variables = { companyId: 'company1', hirerId: 'hirer1' }
+const variables = { companyId: 'company1', hirerId: 'hirer2' }
 
 describe('Company.deleteHirer', () => {
   let result
@@ -31,12 +32,14 @@ describe('Company.deleteHirer', () => {
         {
           id: 'hirer1',
           person: 'person1',
-          company: 'company1'
+          company: 'company1',
+          type: hirerTypes.ADMIN
         },
         {
           id: 'hirer2',
           person: 'person2',
-          company: 'company1'
+          company: 'company1',
+          type: hirerTypes.ADMIN
         }
       ],
       people: [
@@ -58,9 +61,10 @@ describe('Company.deleteHirer', () => {
   it('deletes the hirer', async () => {
     expect(db.hirers.length).to.equal(1)
     expect(db.hirers[0]).to.deep.equal({
-      id: 'hirer2',
-      person: 'person2',
-      company: 'company1'
+      id: 'hirer1',
+      person: 'person1',
+      company: 'company1',
+      type: hirerTypes.ADMIN
     })
   })
 
@@ -75,7 +79,146 @@ describe('Company.deleteHirer', () => {
 
   it('returns the deleted hirer', () => {
     expect(result.data.company.deleteHirer).to.deep.equal({
-      id: 'hirer1'
+      id: 'hirer2'
+    })
+  })
+
+  describe('when the user is not an admin', () => {
+    beforeEach(() => {
+      db = {
+        ...db,
+        hirers: [
+          {
+            id: 'hirer1',
+            person: 'person1',
+            company: 'company1',
+            type: hirerTypes.MEMBER
+          },
+          {
+            id: 'hirer2',
+            person: 'person2',
+            company: 'company1',
+            type: hirerTypes.ADMIN
+          }
+        ]
+      }
+    })
+
+    it('throws an error', async () => {
+      const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      shouldRespondWithGqlError({
+        message: 'User does not have permission to delete teammates',
+        locations: [{ line: 4, column: 7 }],
+        path: [
+          'company',
+          'deleteHirer'
+        ]
+      })(result)
+    })
+
+    it('does not delete the hirer', async () => {
+      await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      expect(db.hirers.length).to.equal(2)
+    })
+  })
+
+  describe('when the user is not a hirer', () => {
+    beforeEach(() => {
+      db = {
+        ...db,
+        hirers: [
+          {
+            id: 'hirer2',
+            person: 'person2',
+            company: 'company1',
+            type: hirerTypes.ADMIN
+          }
+        ]
+      }
+    })
+
+    it('throws an error', async () => {
+      const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      shouldRespondWithGqlError({
+        message: 'User does not have permission to delete teammates',
+        locations: [{ line: 4, column: 7 }],
+        path: [
+          'company',
+          'deleteHirer'
+        ]
+      })(result)
+    })
+  })
+
+  describe('when the targeted hirer is from a different company', () => {
+    beforeEach(() => {
+      db = {
+        ...db,
+        hirers: [
+          {
+            id: 'hirer1',
+            person: 'person1',
+            company: 'company1',
+            type: hirerTypes.MEMBER
+          },
+          {
+            id: 'hirer2',
+            person: 'person2',
+            company: 'company1ButDifferent',
+            type: hirerTypes.ADMIN
+          }
+        ]
+      }
+    })
+
+    it('throws an error', async () => {
+      const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      shouldRespondWithGqlError({
+        message: 'Cannot delete hirers from other companies',
+        locations: [{ line: 4, column: 7 }],
+        path: [
+          'company',
+          'deleteHirer'
+        ]
+      })(result)
+    })
+
+    it('does not delete the hirer', async () => {
+      await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      expect(db.hirers.length).to.equal(2)
+    })
+  })
+
+  describe('when the targeted hirer is the current user', () => {
+    beforeEach(() => {
+      db = {
+        ...db,
+        hirers: [
+          {
+            id: 'hirer2',
+            person: 'person1',
+            company: 'company1',
+            type: hirerTypes.ADMIN
+          }
+        ]
+      }
+    })
+
+    it('throws an error', async () => {
+      const result = await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      shouldRespondWithGqlError({
+        message: 'Cannot delete self',
+        locations: [{ line: 4, column: 7 }],
+        path: [
+          'company',
+          'deleteHirer'
+        ]
+      })(result)
+    })
+
+    it('does not delete the hirer', async () => {
+      await executeQueryOnDbUsingSchema({ operation, db, schema, variables })
+      expect(db.hirers.length).to.equal(1)
     })
   })
 })

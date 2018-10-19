feb51ca6089b94a0a9880fabf3e8575fdac26094
Timothy Robinson <timmyrobby@gmail.com>
2018-10-19 12:00:24 +0100

    minor: Add `Company.application` resolver

---
+++ src/gql/schema/company/application.js
@@ -0,0 +1,30 @@
+module.exports = {
+  typeDefs: `
+    extend type Company {
+      application (id: ID!): Application
+    }
+  `,
+  resolvers: {
+    Company: {
+      application: async (company, args, context) => {
+        const { id: applicationId } = args
+
+        const application = await context.store.readOne({
+          type: 'applications',
+          id: applicationId
+        })
+        if (!application) return null
+
+        const job = await context.store.readOne({
+          type: 'jobs',
+          id: application.job
+        })
+
+        if (job.company !== company.id) {
+          return null
+        }
+        return application
+      }
+    }
+  }
+}

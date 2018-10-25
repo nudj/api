29d93ea5ebe6d91e7932cc727675c00707b89bf4
Nick Collings <nick@collingo.com>
2018-10-25 12:25:59 +0100

    minor: Add Person.getOrCreatePersonAndReferral

---
+++ src/gql/schema/job/getOrCreatePersonAndReferral.js
@@ -0,0 +1,54 @@
+const omitBy = require('lodash/omitBy')
+
+module.exports = {
+  typeDefs: `
+    extend type Job {
+      getOrCreatePersonAndReferral(
+        person: PersonCreateInput!
+        parent: ID
+      ): Referral
+    }
+  `,
+  resolvers: {
+    Job: {
+      getOrCreatePersonAndReferral: async (job, args, context) => {
+        const { parent: parentId } = args
+
+        const [
+          person,
+          parent
+        ] = await Promise.all([
+          context.store.readOneOrCreate({
+            type: 'people',
+            filters: { email: args.person.email },
+            data: omitBy(args.person, ['company', 'role'])
+          }),
+          parentId && context.store.readOne({
+            type: 'referrals',
+            id: parentId
+          })
+        ])
+
+        const referralData = {
+          job: job.id,
+          person: person.id
+        }
+
+        if (parent) {
+          referralData.parent = parent.id
+        }
+
+        const referral = await context.store.readOneOrCreate({
+          type: 'referrals',
+          filters: {
+            job: job.id,
+            person: person.id
+          },
+          data: referralData
+        })
+
+        return referral
+      }
+    }
+  }
+}

62ef658171f1e3e4d8fec97683085a8a46f85861
Nick Collings <nick@collingo.com>
2018-10-31 15:48:58 +0000

    patch: Add trim to new company args (strings only) before creation

--- src/gql/schema/person/addCompanyAndAssignUserAsHirer.js
+++ src/gql/schema/person/addCompanyAndAssignUserAsHirer.js
@@ -1,4 +1,6 @@
 const generateHash = require('hash-generator')
+const mapValues = require('lodash/mapValues')
+
 const { values: hirerTypes } = require('../enums/hirer-types')
 const { values: dataSources } = require('../enums/data-sources')
 const { validateCompanyCreation } = require('../../lib/helpers/validation/company')
@@ -14,14 +16,14 @@ module.exports = {
   resolvers: {
     Person: {
       addCompanyAndAssignUserAsHirer: async (person, args, context) => {
-        await validateCompanyCreation(args.company, context)
-        const companyName = args.company.name
+        const companyArgs = mapValues(args.company, value => value.trim ? value.trim() : value)
+        await validateCompanyCreation(companyArgs, context)
 
         // Avoid using `readOneOrCreate` to prevent unnecessarily generating a
         // slug and checking newly created companies for existing hirers.
         let company = await context.sql.readOne({
           type: 'companies',
-          filters: { name: companyName }
+          filters: { name: companyArgs.name }
         })
 
         if (company) {
@@ -44,7 +46,7 @@ module.exports = {
             }
           })
         } else {
-          company = await createCompany(context, args.company, {
+          company = await createCompany(context, companyArgs, {
             createDummyData: true
           })
         }
@@ -91,7 +93,6 @@ module.exports = {
           }
         })
         await updateIntercomTagsForHirer(context, hirer)
-
         return hirer
       }
     }
